fe0cb3eeb290d30442673195b672a9b5a7b41cda
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-06-15 15:05:30 +0100

    New js and css for the secondary nav
    Added us the new js to control for the nav and its inclusion in the libraries file to be found along with the new classes which is added as part of the js

---
+++ assets/js/nav-scroll.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    el_scrollNav = document.querySelector('.scroll-nav');
+
+    if(el_scrollNav) {
+        var last_scroll_top = 0;
+        window.addEventListener('scroll', function(){
+            let scroll_top = window.scrollY;
+            if(scroll_top < last_scroll_top) {
+                el_scrollNav.classList.remove('scrolled-down');
+                el_scrollNav.classList.add('scrolled-up');
+            }
+            else {
+                el_scrollNav.classList.remove('scrolled-up');
+                el_scrollNav.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+        });
+    }
+});
