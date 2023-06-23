ac92f01a571c0e7f8d399537bc6fdbb46d8c82c5
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-06-23 13:07:34 +0100

    Adding 2 new js to the theme and lirbaries

---
+++ assets/js/nav-btn.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var x = document.getElementById('myNav');
+    var y = document.getElementById('main-nav');
+    document.querySelector("#overlay-btn").addEventListener('click', 
+      function navBtn() {
+        document.getElementById('overlay-btn').classList.toggle("change");
+        if (x.style.height === "100%") {
+          x.style.height = "0%";
+          document.querySelector('.main').classList.remove('active');
+          document.querySelector("body").classList.remove('noscroll');
+          document.querySelector('#overlay-btn').classList.add('overlay-btn');
+          document.querySelector('.search-block').classList.remove('overlay-search');
+          document.querySelector('.navigation_col').classList.remove('overlay-menu');
+          document.querySelector(y).classList.add('fixed-top');
+          } else {
+            x.style.height = "100%";
+            if(document.querySelector('#prodForm').classList.contains('in')) {
+              document.querySelector('#prodForm').modal('hide')
+            }
+            if(document.querySelector('#resForm').classList.contains('in')) {
+              document.querySelector('#resForm').modal('hide')
+            }
+            if(document.querySelector('#location-details').dialog('isOpen') === true) {
+              document.querySelector('#location-details').dialog('close');
+            }
+            document.querySelector('.main').classList.add('active');
+            document.querySelector("body").classList.add('noscroll');
+            document.querySelector('#overlay-btn').classList.remove('overlay-btn');
+            document.querySelector('.search-block').classList.add('overlay-search');
+            document.querySelector('.navigation_col').classList.add('overlay-menu');
+            document.querySelector(y).classList.remove('fixed-top');
+          }
+      });
+  
+      document.querySelector("#tablet-overlay-btn").addEventListener('click',
+        function navBtn() {
+          document.getElementById('tablet-overlay-btn').classList.toggle("change");
+          if (x.style.height === "100%") {
+              x.style.height = "0%";
+              document.querySelector('.main').classList.remove('active');
+              document.querySelector("body").classList.remove('noscroll');
+              document.querySelector('#overlay-btn').classList.add('overlay-btn');
+              document.querySelector('.search-block').classList.remove('overlay-search');
+              document.querySelector('.navigation_col').classList.remove('overlay-menu');
+              document.querySelector(y).classList.add('fixed-top');
+            } else {
+              x.style.height = "100%";
+              if(document.querySelector('#prodForm').classList.contains('in')) {
+                document.querySelector('#prodForm').modal('hide')
+              }
+              if(document.querySelector('#resForm').classList.contains('in')) {
+                document.querySelector('#resForm').modal('hide')
+              }
+              if(document.querySelector('#location-details').dialog('isOpen') === true) {
+                document.querySelector('#location-details').dialog('close');
+              }
+              document.querySelector('.main').classList.add('active');
+              document.querySelector("body").classList.add('noscroll');
+              document.querySelector('#overlay-btn').classList.remove('overlay-btn');
+              document.querySelector('.search-block').classList.add('overlay-search');
+              document.querySelector('.navigation_col').classList.add('overlay-menu');
+              document.querySelector(y).classList.remove('fixed-top');
+            }
+        });
+  });
+  
