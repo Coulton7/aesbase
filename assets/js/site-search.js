8733a95083f81b7db33a824e07d209af099e1643
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-07-26 12:45:28 +0100

    Adding the inital js and css for angolia search for testing

---
+++ assets/js/site-search.js
@@ -0,0 +1,18 @@
+const searchClinet = algoliasearch ('ZUQNGEX563', '23e29710cc4469dec35bd50bc2164b3a');
+
+const search = instantsearch({
+    indexName: 'aesseal',
+    searchClinet,
+});
+
+search.addWidgets ([
+    instantsearch.widgets.searchBox({
+        container: '#searchbox',
+    }),
+
+    instantsearch.widgets.hits ({
+        container: 'hits',
+    })
+]);
+
+search.start();
