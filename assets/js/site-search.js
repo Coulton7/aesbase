401a9493a4d0f6d103fa469f57bd06100caa310d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-07-26 14:37:36 +0100

    Removing the space from the js

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1,4 +1,4 @@
-const searchClinet = algoliasearch ('ZUQNGEX563', '23e29710cc4469dec35bd50bc2164b3a');
+const searchClinet = algoliasearch('ZUQNGEX563', '23e29710cc4469dec35bd50bc2164b3a');
 
 const search = instantsearch({
     indexName: 'aesseal',
