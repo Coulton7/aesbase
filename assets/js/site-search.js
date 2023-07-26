72051f0a4243c03e8003e7e9f35e661b2c55e340
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-07-26 14:56:18 +0100

    More js set up for the search

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -3,15 +3,30 @@ const searchClinet = algoliasearch('ZUQNGEX563', '23e29710cc4469dec35bd50bc2164b
 const search = instantsearch({
     indexName: 'aesseal',
     searchClinet,
+    routing: true,
 });
 
+search.addWidgets([
+    instantsearch.widgets.configure({
+        hitsPerPage: 10,
+    })
+])
+
 search.addWidgets ([
+
     instantsearch.widgets.searchBox({
         container: '#searchbox',
+        placeholder: 'Enter Your Keywords'
     }),
+])
 
+search.addWidgets([
     instantsearch.widgets.hits ({
         container: '#hits',
+        templates: {
+            item: document.getElementById('hit-template').innerHTML,
+            empty: `We didn't find any results for the search <em>"{{query}}"</em>`,
+          },
     })
 ]);
 
