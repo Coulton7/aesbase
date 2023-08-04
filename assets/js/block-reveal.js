302a3f6955e977909fedf7c5cff85a5214372333
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-08-04 09:32:54 +0100

    Adding new js at the ready needs to be added to the library

---
+++ assets/js/block-reveal.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function getUrlVars()
+        {
+        var option = [], tag;
+        var tags = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < tags.length; i++)
+        {
+            tag = tags[i].split('=');
+            option.push(tag[0]);
+            option[tag[0]] = tag[1];
+        }
+        return option;
+        }
+
+        var salesString = getUrlVars()["s"];
+        if(salesString == 'ml') {
+        document.getElementById('sales-ml').style.display = 'block';
+        document.getElementById('sales').style.display = 'none';
+        }
+        else if(salesString == 'sj') {
+        document.getElementById('sales-sj').style.display = 'block';
+        document.getElementById('sales').style.display = 'none';
+        }
+        else if(salesString == 'rs') {
+        document.getElementById('sales-rs').style.display = 'block';
+        document.getElementById('sales').style.display = 'none';
+        }
+});
