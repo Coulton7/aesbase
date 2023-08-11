613b365db2d70127d46b2fd610fe67157d100a4b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-08-11 12:03:16 +0100

    New features on the js to hide fields when empty

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1,54 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
-const searchClient = algoliasearch('ZUQNGEX563', '23e29710cc4469dec35bd50bc2164b3a');
-
-const search = instantsearch({
-    indexName: 'aesseal',
-    searchClient,
-    routing: true,
-});
-
-search.addWidgets([
-    instantsearch.widgets.configure({
-        hitsPerPage: 20,
-        attributesToSnippet: ['description:40'],
-        page: 0,
-    }),
-
-    instantsearch.widgets.pagination({
-        container: '#pagination',
-        totalPages: 3,
-        scrollTo: '#searchbox'
-    }),
-
-    instantsearch.widgets.searchBox({
-        container: '#searchbox',
-        placeholder: 'Enter Your Keywords',
-        searchAsYouType: false,
-        cssClasses: {
-            form : 'search-block',
-            input: 'form-control',
-            submit: 'btn btn-primary',
-            submitIcon: 'white-text'
-        }
-    }),
-
-    instantsearch.widgets.hits ({
-        container: '#hits',
-        templates:{
-            item: data => `
-            <div class="search-result">
-                <p class="h3">${data.title}</p>
-                <p class="h3">${data.name_1}</p>
-                <p class="lead">${data.type}</p>
-                <p>${data.description}</p>
-                <p>${data.body}</p>
-            </div>`,
-            empty: `<p class="h3">No results found matching {{query}}</p>
-            <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>'
-            <p class="h3">Are you searching for a Part Number or Serial Number?</p>`,
-        }
-    })
-]);
-
-search.start();
-});
+    const searchClient = algoliasearch('ZUQNGEX563', '23e29710cc4469dec35bd50bc2164b3a');
+    
+    const search = instantsearch({
+        indexName: 'aesseal',
+        searchClient,
+        routing: true,
+    });
+    
+    search.addWidgets([
+        instantsearch.widgets.configure({
+            hitsPerPage: 20,
+            attributesToSnippet: ['description:40'],
+            page: 0,
+        }),
+    
+        instantsearch.widgets.pagination({
+            container: '#pagination',
+            totalPages: 3,
+            scrollTo: '#searchbox'
+        }),
+    
+        instantsearch.widgets.searchBox({
+            container: '#searchbox',
+            placeholder: 'Enter Your Keywords',
+            searchAsYouType: false,
+            cssClasses: {
+                form : 'search-block',
+                input: 'form-control',
+                submit: 'btn btn-primary',
+                submitIcon: 'white-text'
+            }
+        }),
+    
+        instantsearch.widgets.hits ({
+            container: '#hits',
+            templates:{
+                item: data => `
+                <div class="search-result">
+                    <p class="h3 ${data.title ? '' : 'd-none'}">${data.title}</p>
+                    <p class="h3 ${data.name_1 ? '' : 'd-none'}">${data.name_1}</p>
+                    <p class="lead">${data.type}</p>
+                    <p class=${data.description ? '' : 'd-none'}>${data.description}</p>
+                    <p class=${data.body ? '' : 'd-none'}>${data.body}</p>
+                </div>`,
+                empty: `<p class="h3">No results found matching {{query}}</p>
+                <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>'
+                <p class="h3">Are you searching for a Part Number or Serial Number?</p>`,
+            }
+        })
+    ]);
+    
+    search.start();
+    });
