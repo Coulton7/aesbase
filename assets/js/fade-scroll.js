2a13a8a94d9e8c4ed4d4ec7c1971c21b1b47c5fe
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-09-28 12:47:51 +0100

    Trying a small adjustment to the js to see if it works

--- assets/js/fade-scroll.js
+++ assets/js/fade-scroll.js
@@ -5,31 +5,35 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbar = document.querySelector('.bg-transparent .navbar-collapse');
     var navLinks = document.querySelectorAll('.navbar-nav a.nav-link');
     
+    function fadeScroll() {
+        window.addEventListener('scroll', function(e) {
+            var scroll = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+            sectionShadow.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
+            textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
+            enquiry.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
+            navbar.style.backgroundColor = "rgba(255,255,255,"+ Math.max(0, Math.min(1, +scroll / 600)); +")"
+    
+            if(scroll/600 >= 0.75) {
+                for (var i = 0; i < navLinks.length; i++) {
+                    navLinks[i].classList.add('normText');
+                }
+            }
+    
+            if(scroll/600 < 0.75) {
+                for (var i = 0; i < navLinks.length; i++) {
+                    navLinks[i].classList.remove('normText');
+                }
+            }
+        });
+    };
+
     if(window.innerWidth > 992) {
         sectionShadow.style.opacity = -1.03;
         textLogo.style.opacity = -1.03;
         enquiry.style.opacity = -1.03;
         navbar.style.backgroundColor = "rgba(255,255,255,0)";
+        fadeScroll();
 
-    window.addEventListener('scroll', function(e) {
-        var scroll = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-        sectionShadow.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-        textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-        enquiry.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-        navbar.style.backgroundColor = "rgba(255,255,255,"+ Math.max(0, Math.min(1, +scroll / 600)); +")"
-
-        if(scroll/600 >= 0.75) {
-            for (var i = 0; i < navLinks.length; i++) {
-                navLinks[i].classList.add('normText');
-            }
-        }
-
-        if(scroll/600 < 0.75) {
-            for (var i = 0; i < navLinks.length; i++) {
-                navLinks[i].classList.remove('normText');
-            }
-        }
-    });
     } else {
         sectionShadow.style.opacity = 1;
         textLogo.style.opacity = 1;
@@ -43,22 +47,8 @@ document.addEventListener("DOMContentLoaded", function() {
             textLogo.style.opacity = -1.03;
             enquiry.style.opacity = -1.03;
             navbar.style.backgroundColor = "rgba(255,255,255,0)";
+            fadeScroll();
         
-            window.addEventListener('scroll', function(e) {
-                var scroll = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-                sectionShadow.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-                textLogo.style.opacity = Math.max(0, Math.min(1, +scroll / 600));
-                enquiry.style.opacity = Math.max(0, Math.min(1, +scroll / 500));
-                navbar.style.backgroundColor = "rgba(255,255,255,"+ Math.max(0, Math.min(1, +scroll / 600)); +")"
-
-                if(scroll/600 >= 0.75) {
-                    document.querySelector('.navbar-nav a').classList.add('normText');
-                }
-        
-                if(scroll/600 < 0.75) {
-                    document.querySelector('.navbar-nav a').classList.remove('normText');
-                }
-            });
         } else {
             sectionShadow.style.opacity = 1;
             textLogo.style.opacity = 1;
