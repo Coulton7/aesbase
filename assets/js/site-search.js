955b1f6045f6291fd6a494c4d71a42273a3b8341
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-10-17 14:06:42 +0100

    Removed for now need to apply to hits first

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -58,7 +58,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 });
                 return items;
             },
-            sortBy : ['isRefined:desc'],
         }),
     
         instantsearch.widgets.pagination({
