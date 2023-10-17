02c0e665d6aa3c87a0ec92497d0a90162c4351a4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-10-17 14:02:45 +0100

    Adding a sort by for testing it

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -58,6 +58,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 });
                 return items;
             },
+            sortBy : ['isRefined:desc'],
         }),
     
         instantsearch.widgets.pagination({
