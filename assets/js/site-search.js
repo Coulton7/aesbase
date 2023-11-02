2cf48350d0c14a5a97556d4fd3e484cbe72fbe2b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-11-02 14:19:34 +0000

    Mapping across correct search updates

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -19,6 +19,27 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    const typeMapping = {
+        'article': 'Article',
+        'apiplans': 'API Plans',
+        'cartridgemechanicalseals': 'Cartridge Mechanical Seals',
+        'sealsupportsystems': 'Seal Support Systems',
+        'glandpacking': 'Gland Packing',
+        'locations': 'Locations',
+        'page': 'Web Page',
+        'video': 'Video',
+        'gasseals': 'Gas Seals',
+        'componentseals': 'Component Seals',
+        'bearingprotection': 'Bearing Protection',
+        'academy': 'Academy',
+        'elastomers': 'Elastomers',
+        'productbrochure': 'Product Brochure'
+    }
+
+    const vidMapping ={
+        'industry': 'Industry'
+    }
+
     const langlistPanel = instantsearch.widgets.panel ({
         templates: {
             header: '<h4>Select your Language</h4>'
@@ -103,6 +124,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 header: 'Filter by Content Type',
                 item: '<input type="checkbox" class="ais-refinement-list--checkbox" {{#isRefined}}checked="true"{{/isRefined}}> {{label}} <span class="ais-refinement-list--count">({{count}})</span>',
             },
+            transformItems(items){
+                return items.map(item => ({
+                    ...item,
+                    label: typeMapping[item.label],
+                }));
+            },
             cssClasses: {
                 item: ['types-item']
             },
@@ -149,86 +176,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 empty: `<p class="h3">No results found matching {{query}}</p>
                 <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>'
                 <p class="h3">Are you searching for a Part Number or Serial Number?</p>`,
+            },
+            transformItems(items){
+                return items.map(item => ({
+                    ...item,
+                    type: typeMapping[item.type],
+                    vid: vidMapping[item.vid]
+                }))
             }
         })
     ]);
     
     search.start();
-
-    search.on('render', function() {
-        var contentType = document.querySelectorAll('#contentCat');
-        var vocabName = document.querySelectorAll("#vocabCat");
-        var refineType = document.querySelectorAll(".types-item");
-        for (var i = 0; i < contentType.length; i++) {
-
-            if(contentType[i].textContent == 'article') {
-                contentType[i].textContent = contentType[i].textContent.replace("article", "Article");
-            } else if (contentType[i].textContent == 'page') {
-                contentType[i].textContent = contentType[i].textContent.replace("page","Web Page");
-            } else if (contentType[i].textContent == 'apiplans') {
-                contentType[i].textContent = contentType[i].textContent.replace("apiplans","API Plans");
-            } else if (contentType[i].textContent == 'cartridgemechanicalseals') {
-                contentType[i].textContent = contentType[i].textContent.replace("cartridgemechanicalseals","Cartridge Mechanical Seals");
-            } else if (contentType[i].textContent == 'gasseals') {
-                contentType[i].textContent = contentType[i].textContent.replace("gasseals","Gas Seals");
-            } else if (contentType[i].textContent == 'sealsupportsystems') {
-                contentType[i].textContent = contentType[i].textContent.replace("sealsupportsystems","Seal Support Systems");
-            } else if (contentType[i].textContent == 'bearingprotection') {
-                contentType[i].textContent = contentType[i].textContent.replace("bearingprotection","Bearing Protection");
-            } else if (contentType[i].textContent == 'componentseals') {
-                contentType[i].textContent = contentType[i].textContent.replace("componentseals","Component Seals");
-            } else if (contentType[i].textContent == 'glandpacking') {
-                contentType[i].textContent = contentType[i].textContent.replace("glandpacking","Gland Packing");
-            } else if (contentType[i].textContent == 'elastomers') {
-                contentType[i].textContent = contentType[i].textContent.replace("elastomers","Elastomers");
-            } else if (contentType[i].textContent == 'video') {
-                contentType[i].textContent = contentType[i].textContent.replace("video","Video");
-            } else if (contentType[i].textContent == 'academy') {
-                contentType[i].textContent = contentType[i].textContent.replace("academy","Academy");
-            } else if (contentType[i].textContent == 'locations') {
-                contentType[i].textContent = contentType[i].textContent.replace("locations","Locations");
-            } else if (contentType[i].textContent == 'undefined') {
-                contentType[i].textContent = contentType[i].textContent.replace("undefined","");
-            }
-        }
-
-        for (var x = 0; x < refineType.length; x++) {
-
-            if(refineType[x].innerHTML.includes('article')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace('article', 'Article');
-            } else if (refineType[x].innerHTML.includes('page')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("page","Web Page");
-            } else if (refineType[x].innerHTML.includes('apiplans')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("apiplans","API Plans");
-            } else if (refineType[x].innerHTML.includes('cartridgemechanicalseals')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("cartridgemechanicalseals","Cartridge Mechanical Seals");
-            } else if (refineType[x].innerHTML.includes('gasseals')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("gasseals","Gas Seals");
-            } else if (refineType[x].innerHTML.includes('sealsupportsystems')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("sealsupportsystems","Seal Support Systems");
-            } else if (refineType[x].innerHTML.includes('bearingprotection')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("bearingprotection","Bearing Protection");
-            } else if (refineType[x].innerHTML.includes('componentseals')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("componentseals","Component Seals");
-            } else if (refineType[x].innerHTML.includes('glandpacking')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("glandpacking","Gland Packing");
-            } else if (refineType[x].innerHTML.includes('elastomers')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("elastomers","Elastomers");
-            } else if (refineType[x].innerHTML.includes('video')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("video","Video");
-            } else if (refineType[x].innerHTML.includes('academy')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("academy","Academy");
-            } else if (refineType[x].innerHTML.includes('locations')) {
-                refineType[x].innerHTML = refineType[x].innerHTML.replace("locations",'Locations');
-            }
-        }
-
-        for (var y = 0; y < vocabName.length; y++) {
-            if(vocabName[y].textContent == 'industry') {
-                vocabName[y].textContent = vocabName[y].textContent.replace("industry","Industry")
-            } else if (vocabName[y].textContent == 'undefined') {
-                vocabName.textContent = ""
-            }
-        }
-    })
 });
