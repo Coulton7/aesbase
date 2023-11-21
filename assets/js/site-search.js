c552b12bd6156c72414e0bf921322f31aaf42891
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-11-21 10:35:41 +0000

    Adding some new classes for the read more link

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -192,7 +192,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         attribute: "body",
                         hit: data
                     })}</p>
-                    <a href="${data.url}">Read More</a>
+                    <a class="btn btn-primary d-flex justify-content-end" href="${data.url}">Read More</a>
                 </div>`,
                 empty: `<p class="h3">No results found matching {{query}}</p>
                 <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>'
