ebf7ee87943d684d7719a47fdd59da5b8d0b7a54
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-01-29 15:39:30 +0000

    Adding small changes to the js need later time to test

--- assets/js/form-fill.js
+++ assets/js/form-fill.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.submit-notice').style.display = 'none';
-    if (document.querySelector('.field-hubspot-form').length > 0) {
+    if (document.querySelector('.hubspot-form').length > 0) {
       document.querySelector('.download-field-wrapper').style.display = 'none';
     }
   
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var submitString = getUrlVars()["fs"];
     if(submitString=='y'){
         document.querySelector('.download-field-wrapper').style.display = 'block';
-        document.querySelector('.field-hubspot-form').style.display = 'none';
+        document.querySelector('.hubspot-form').style.display = 'none';
         document.querySelector('.field-label').style.display = 'none';
         document.querySelector('.submit-notice').style.display = 'block';
     }
