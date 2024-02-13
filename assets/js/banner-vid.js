a0ebd663551565b633a957f264c72108adf253f0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-02-13 10:30:15 +0000

    Adding if statement to see if it prevents any errors

--- assets/js/banner-vid.js
+++ assets/js/banner-vid.js
@@ -1,24 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const bannerVid = document.getElementById("bannerVid");
-  if (window.width > 991) {
-    bannerVid.getElementsByTagName("source");
+  if(document.getElementById("bannerVid")){
+    const bannerVid = document.getElementById("bannerVid");
+    if (window.width > 991) {
+      bannerVid.getElementsByTagName("source");
 
-    for (let i =0; i< bannerVid.clientHeight; ++i) {
-      bannerVid[i].src = bannerVid[i].dataset.src;
+      for (let i =0; i< bannerVid.clientHeight; ++i) {
+       bannerVid[i].src = bannerVid[i].dataset.src;
+      }
     }
-  }
 
-  bannerVid.load();
+    bannerVid.load();
+  }
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-  const lowerwave = document.getElementsByClassName('lower-wave');
-  const bannerVid = document.getElementsByClassName('banner-vid');
+  if(document.getElementById("bannerVid")){
+    const lowerwave = document.getElementsByClassName('lower-wave');
+    const bannerVid = document.getElementsByClassName('banner-vid');
 
-  const lowerwaveRect = lowerwave[0].getBoundingClientRect();
+    const lowerwaveRect = lowerwave[0].getBoundingClientRect();
 
-  const lowerTop = lowerwaveRect.height;
+    const lowerTop = lowerwaveRect.height;
 
-  bannerVid[0].style.bottom = '-' + (lowerTop + 30) + 'px';
-
+    bannerVid[0].style.bottom = '-' + (lowerTop + 30) + 'px';
+  }
 });
