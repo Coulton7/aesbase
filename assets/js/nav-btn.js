5fa08e3fd3a5f550c2fa1dd8734d0bf21783ac73
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-04-11 12:26:45 +0100

    Small sort for now mob-overlay-search

--- assets/js/nav-btn.js
+++ assets/js/nav-btn.js
@@ -126,7 +126,39 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.querySelector(".dialog-off-canvas-main-canvas").classList.add('noscroll');
                 document.querySelector('#overlay-btn').classList.remove('overlay-btn');
               }
-          });
+        });
+
+        document.querySelector('#mob-overlay-search').addEventListener('click', 
+        function searchBtn(){
+          document.getElementById('mob-overlay-search').classList.toggle('change');
+          if (y.style.height === "100%") {
+            y.style.height = "1px";
+            y.style.bottom = "-1px";
+            document.querySelector("html").classList.remove('noscroll');
+            document.querySelector("body").classList.remove('noscroll');
+            document.querySelector(".dialog-off-canvas-main-canvas").classList.remove('noscroll');
+            document.querySelector('#overlay-search').classList.add('search-btn');
+          } else {
+            y.style.height = "100%";
+            y.style.bottom = "0";
+            if (x.style.height == "100%") {
+              x.style.height = "1px";
+              x.style.bottom = "-1px";
+              document.querySelector('#overlay-search').classList.remove('change');
+              document.querySelector('#overlay-search').classList.add('overlay-btn')
+            } else if (s.style.height == "100%") {
+              s.style.height = "1px";
+              s.style.bottom = "-1px";
+              document.querySelector('#overlay-share').classList.remove('change');
+              document.querySelector('#overlay-share').classList.add('share-btn')
+            }
+            document.querySelector("html").classList.add('noscroll');
+            document.querySelector("body").classList.add('noscroll');
+            document.querySelector(".dialog-off-canvas-main-canvas").classList.add('noscroll');
+            document.querySelector('#overlay-search').classList.remove('search-btn');
+          }
+
+        });
 
       var tabletOverlayBtn = document.querySelector("#tablet-overlay-btn");
 
