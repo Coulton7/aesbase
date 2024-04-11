038e87b0864f62b7ecc3a24cde7a098ec8dd0524
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-04-11 09:26:08 +0100

    Change to the overlay btn on the footer to trigger overlay

--- assets/js/nav-btn.js
+++ assets/js/nav-btn.js
@@ -97,6 +97,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
         });
 
+        document.querySelector("#footer-overlay-btn").addEventListener('click', 
+          function footerNavBtn() {
+            document.getElementById('overlay-btn').classList.toggle("change");
+            if (x.style.height === "100%") {
+              x.style.height = "1px";
+              x.style.bottom = "-1px";
+              document.querySelector("html").classList.remove('noscroll');
+              document.querySelector("body").classList.remove('noscroll');
+              document.querySelector(".dialog-off-canvas-main-canvas").classList.remove('noscroll');
+              document.querySelector('#overlay-btn').classList.add('overlay-btn');
+              } else {
+                x.style.height = "100%";
+                x.style.bottom = "0";
+                if (y.style.height == "100%") {
+                  y.style.height = "1px";
+                  y.style.bottom = "-1px";
+                  document.querySelector('#overlay-search').classList.remove('change');
+                  document.querySelector('#overlay-search').classList.add('search-btn')
+                } else if (s.style.height == "100%") {
+                  s.style.height = "1px";
+                  s.style.bottom = "-1px";
+                  document.querySelector('#overlay-share').classList.remove('change');
+                  document.querySelector('#overlay-share').classList.add('share-btn')
+                }
+                document.querySelector("html").classList.add('noscroll');
+                document.querySelector("body").classList.add('noscroll');
+                document.querySelector(".dialog-off-canvas-main-canvas").classList.add('noscroll');
+                document.querySelector('#overlay-btn').classList.remove('overlay-btn');
+              }
+          });
+
       var tabletOverlayBtn = document.querySelector("#tablet-overlay-btn");
 
       if(tabletOverlayBtn !=null ) {
