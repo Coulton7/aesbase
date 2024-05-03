0d227fb0b236b22d7c6c5a59e822f691a7d78f80
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-05-03 10:49:33 +0100

    More specific targeting method

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
             clearButton.textContent = 'X';
 
             searchButton.addEventListener('click', event => {
-                refine(event.input.value);
+                refine(event.querySelector('input').value);
             });
 
             clearButton.addEventListener('click', () => {
