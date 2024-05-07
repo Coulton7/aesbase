d17ce1727ff03e1d540dd979ec841dfb586582bc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-05-07 15:30:47 +0100

    Change to the formatting of the js to be more readable

--- assets/js/nav-btn.js
+++ assets/js/nav-btn.js
@@ -28,9 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         document.querySelector("html").classList.add("noscroll");
         document.querySelector("body").classList.add("noscroll");
-        document
-          .querySelector(".dialog-off-canvas-main-canvas")
-          .classList.add("noscroll");
+        document.querySelector(".dialog-off-canvas-main-canvas").classList.add("noscroll");
         document.querySelector("#overlay-btn").classList.remove("overlay-btn");
       }
     });
@@ -51,9 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
           x.style.height = "1px";
           x.style.bottom = "-1px";
           document.querySelector("#overlay-search").classList.remove("change");
-          document
-            .querySelector("#overlay-search")
-            .classList.add("overlay-btn");
+          document.querySelector("#overlay-search").classList.add("overlay-btn");
         } else if (s.style.height == "100%") {
           s.style.height = "1px";
           s.style.bottom = "-1px";
@@ -175,12 +171,8 @@ document.addEventListener("DOMContentLoaded", function () {
         if (x.style.height == "100%") {
           x.style.height = "1px";
           x.style.bottom = "-1px";
-          document
-            .querySelector("#mob-overlay-search")
-            .classList.remove("change");
-          document
-            .querySelector("#mob-overlay-search")
-            .classList.add("overlay-btn");
+          document.querySelector("#mob-overlay-search").classList.remove("change");
+          document.querySelector("#mob-overlay-search").classList.add("overlay-btn");
         } else if (s.style.height == "100%") {
           s.style.height = "1px";
           s.style.bottom = "-1px";
@@ -189,12 +181,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         document.querySelector("html").classList.add("noscroll");
         document.querySelector("body").classList.add("noscroll");
-        document
-          .querySelector(".dialog-off-canvas-main-canvas")
-          .classList.add("noscroll");
-        document
-          .querySelector("#overlay-search")
-          .classList.remove("search-btn");
+        document.querySelector(".dialog-off-canvas-main-canvas").classList.add("noscroll");
+        document.querySelector("#overlay-search").classList.remove("search-btn");
       }
   });
 
@@ -203,25 +191,17 @@ document.addEventListener("DOMContentLoaded", function () {
     document
       .querySelector("#tablet-overlay-btn")
       .addEventListener("click", function navBtn() {
-        document
-          .getElementById("tablet-overlay-btn")
-          .classList.toggle("change");
+        document.getElementById("tablet-overlay-btn").classList.toggle("change");
         if (x.style.height === "100%") {
           x.style.height = "0%";
           document.querySelector("body").classList.remove("noscroll");
           document.querySelector("#overlay-btn").classList.add("overlay-btn");
-          document
-            .querySelector(".navigation_col")
-            .classList.remove("overlay-menu");
+          document.querySelector(".navigation_col").classList.remove("overlay-menu");
         } else {
           x.style.height = "100%";
           document.querySelector("body").classList.add("noscroll");
-          document
-            .querySelector("#overlay-btn")
-            .classList.remove("overlay-btn");
-          document
-            .querySelector(".navigation_col")
-            .classList.add("overlay-menu");
+          document.querySelector("#overlay-btn").classList.remove("overlay-btn");
+          document.querySelector(".navigation_col").classList.add("overlay-menu");
         }
       });
   } else {
