ef365c9381c8d991273b903cf435b02662e4af50
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-05-17 14:15:22 +0100

    Removing the old code for tests

--- assets/js/img-enquiry-bg-block.js
+++ assets/js/img-enquiry-bg-block.js
@@ -1,8 +1,3 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var imgSrc =  document.querySelector('.field-product-image .flexslider .slides li:nth-child(1) img').getAttribute('src');
-    document.querySelector('.block-productenquiry').style.backgroundImage =   'url(' + imgSrc + ')';
-  });
-
   document.addEventListener('DOMContentLoaded', function(){
 
     var indImg;
