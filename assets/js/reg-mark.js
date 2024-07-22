19e56576a5273bd3d22cd0486b2a6279a3086714
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-07-22 12:15:08 +0100

    Trying the small code variation

--- assets/js/reg-mark.js
+++ assets/js/reg-mark.js
@@ -2,9 +2,9 @@ const replaceHtml = (element, pattern, replacement) =>
   {
     const { innerHTML } = element;
     return Object.assign(element, {
-      innerHTML: innerHTML.replace(pattern, replacement)
-    })
-  }
+    innerHTML: innerHTML.replace(pattern, replacement)
+  });
+};
 
 document.addEventListener('DOMContentLoaded', function(e){
    replaceHtml(e.target, /<sup>&reg;<\/sup>®/gi, "<sup>&reg;</sup>") 
