e6fedb00280bd2a0aa4039bee2926d25d3c68e0c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-07-23 11:01:34 +0100

    Trying something for the slider on the page

--- assets/js/home-tiny-slider.js
+++ assets/js/home-tiny-slider.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
             activeSlide = info.displayIndex;
             
             total.textContent = info.slideCount;
-            current.textContent = info.displayIndex;
+            current.textContent = info.displayIndex + 2;
     
         newsslider.events.on('transitionEnd', function(info) {
             activeSlide = info.displayIndex;
