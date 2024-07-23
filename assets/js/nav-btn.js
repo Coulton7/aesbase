1741b154b4cf6263db9ea213cb03a1232df754e6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-07-23 14:58:09 +0100

    Update for nav-btn for testing

--- assets/js/nav-btn.js
+++ assets/js/nav-btn.js
@@ -2,222 +2,194 @@ document.addEventListener("DOMContentLoaded", function () {
   var x = document.getElementById("overlayNav");
   var y = document.getElementById("searchOverlay");
   var s = document.getElementById("shareOverlay");
-  var navLinks = document.querySelectorAll('.navbar-nav a.nav-link');
+  var overlayNavBtn = document.querySelector("#overlay-btn");
+  var overlaySearchBtn = document.querySelector("#overlay-search");
+  var overlayShareBtn = document.querySelector("#overlay-share");
+  var html = document.querySelector("html");
+  var body = document.querySelector("body");
+  var dialogOffCanvas = document.querySelector(".dialog-off-canvas-main-canvas");
+  var frontPage = document.querySelector(".path-frontpage");
 
-  document.querySelector("#overlay-btn").addEventListener("click", function navBtn() {
-    document.getElementById("overlay-btn").classList.toggle("change");
-      if (x.style.height === "100%") {
-        x.style.height = "1px";
-        x.style.bottom = "-1px";
-        document.querySelector("html").classList.remove("noscroll");
-        document.querySelector("body").classList.remove("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.remove("noscroll");
-        document.querySelector("#overlay-btn").classList.add("overlay-btn");
-      } else {
-        x.style.height = "100%";
-        x.style.bottom = "0";
-        if (y.style.height == "100%") {
-          y.style.height = "1px";
-          y.style.bottom = "-1px";
-          document.querySelector("#overlay-search").classList.remove("change");
-          document.querySelector("#overlay-search").classList.add("search-btn");
-        } else if (s.style.height == "100%") {
-          s.style.height = "1px";
-          s.style.bottom = "-1px";
-          document.querySelector("#overlay-share").classList.remove("change");
-          document.querySelector("#overlay-share").classList.add("share-btn");
-        }
-        document.querySelector("html").classList.add("noscroll");
-        document.querySelector("body").classList.add("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.add("noscroll");
-        document.querySelector("#overlay-btn").classList.remove("overlay-btn");
-      }
-    });
-
-  document.querySelector("#overlay-search").addEventListener("click", function searchBtn() {
-    document.getElementById("overlay-search").classList.toggle("change");
-      if (y.style.height === "100%") {
-        y.style.height = "1px";
-        y.style.bottom = "-1px";
-        document.querySelector("html").classList.remove("noscroll");
-        document.querySelector("body").classList.remove("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.remove("noscroll");
-        document.querySelector("#overlay-search").classList.add("search-btn");
-        document.querySelector(".scroll-nav").classList.remove("shadow-back");
-        document.querySelector('#autocollapse .navbar-collapse').classList.remove("bg-white");
-        document.querySelector('.overlay-btn').classList.remove("normText");
-        navLinks.forEach(function (navLink) {
-          navLink.classList.remove('normText');
-        });
-      } else {
-        y.style.height = "100%";
-        y.style.bottom = "0";
-        if (x.style.height == "100%") {
-          x.style.height = "1px";
-          x.style.bottom = "-1px";
-          document.querySelector("#overlay-search").classList.remove("change");
-          document.querySelector("#overlay-search").classList.add("overlay-btn");
-        } else if (s.style.height == "100%") {
-          s.style.height = "1px";
-          s.style.bottom = "-1px";
-          document.querySelector("#overlay-share").classList.remove("change");
-          document.querySelector("#overlay-share").classList.add("share-btn");
-        }
-        document.querySelector("html").classList.add("noscroll");
-        document.querySelector("body").classList.add("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.add("noscroll");
-        document.querySelector("#overlay-search").classList.remove("search-btn");
-        document.querySelector(".scroll-nav").classList.add("shadow-back");
-        document.querySelector('#autocollapse .navbar-collapse').classList.add("bg-white");
-        document.querySelector('.overlay-btn').classList.add("normText");
-        navLinks.forEach(function (navLink) {
-          navLink.classList.add('normText');
-        });
-      }
-    });
-
-  document.querySelector("#overlay-share").addEventListener("click", function shareBtn() {
-      document.getElementById("overlay-share").classList.toggle("change");
-      if (s.style.height === "100%") {
-        s.style.height = "1px";
-        s.style.bottom = "-1px";
-        document.querySelector("html").classList.remove("noscroll");
-        document.querySelector("body").classList.remove("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.remove("noscroll");
-        document.querySelector("#overlay-share").classList.add("share-btn");
-      } else {
-        s.style.height = "100%";
-        s.style.bottom = "0";
-        if (y.style.height == "100%") {
-          y.style.height = "1px";
-          y.style.bottom = "-1px";
-          document.querySelector("#overlay-search").classList.remove("change");
-          document.querySelector("#overlay-search").classList.add("search-btn");
-        } else if (s.style.height == "100%") {
-          x.style.height = "1px";
-          x.style.bottom = "-1px";
-          document.querySelector("#overlay-btn").classList.remove("change");
-          document.querySelector("#overlay-btn").classList.add("overlay-btn");
-        }
-        document.querySelector("html").classList.add("noscroll");
-        document.querySelector("body").classList.add("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.add("noscroll");
-        document.querySelector("#overlay-share").classList.remove("share-btn");
-      }
-  });
-
-  document.querySelector("#footer-overlay-btn").addEventListener("click", function footerNavBtn() {
-    document.getElementById("footer-overlay-btn").classList.toggle("change");
-      if (x.style.height === "100%") {
-        x.style.height = "1px";
-        x.style.bottom = "-1px";
-        document.querySelector("html").classList.remove("noscroll");
-        document.querySelector("body").classList.remove("noscroll");
-        document
-          .querySelector(".dialog-off-canvas-main-canvas")
-          .classList.remove("noscroll");
-        document.querySelector("#overlay-btn").classList.add("overlay-btn");
-      } else {
-        x.style.height = "100%";
-        x.style.bottom = "0";
-        if (y.style.height == "100%") {
-          y.style.height = "1px";
-          y.style.bottom = "-1px";
-          document.querySelector("#overlay-search").classList.remove("change");
-          document.querySelector("#overlay-search").classList.add("search-btn");
-        } else if (s.style.height == "100%") {
-          s.style.height = "1px";
-          s.style.bottom = "-1px";
-          document.querySelector("#overlay-share").classList.remove("change");
-          document.querySelector("#overlay-share").classList.add("share-btn");
-        }
-        document.querySelector("html").classList.add("noscroll");
-        document.querySelector("body").classList.add("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.add("noscroll");
-        document.querySelector("#overlay-btn").classList.remove("overlay-btn");
-      }
-  });
-
-  document.querySelector("[data-bs-target='#mobile-overlay'").addEventListener("click", function mobNavBtn() {
-    document.querySelector("[data-bs-target='#mobile-overlay'").classList.toggle("change");
-      if (x.style.height === "100%") {
-        x.style.height = "1px";
-        x.style.bottom = "-1px";
-        document.querySelector("html").classList.remove("noscroll");
-        document.querySelector("body").classList.remove("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.remove("noscroll");
-      } else {
-        x.style.height = "100%";
-        x.style.bottom = "0";
-        if (y.style.height == "100%") {
-          y.style.height = "1px";
-          y.style.bottom = "-1px";
-          document.querySelector("#overlay-search").classList.remove("change");
-          document.querySelector("#overlay-search").classList.add("search-btn");
-        } else if (s.style.height == "100%") {
-          s.style.height = "1px";
-          s.style.bottom = "-1px";
-          document.querySelector("#overlay-share").classList.remove("change");
-          document.querySelector("#overlay-share").classList.add("share-btn");
-        }
-        document.querySelector("html").classList.add("noscroll");
-        document.querySelector("body").classList.add("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.add("noscroll");
-        document.querySelector("[data-bs-target='#mobile-overlay'").classList.remove("overlay-btn");
-      }
-    });
-
-  document.querySelector("#mob-overlay-search").addEventListener("click", function mobSearchBtn() {
-    document.getElementById("mob-overlay-search").classList.toggle("change");
-      if (y.style.height === "100%") {
-        y.style.height = "1px";
-        y.style.bottom = "-1px";
-        document.querySelector("html").classList.remove("noscroll");
-        document.querySelector("body").classList.remove("noscroll");
-        document
-          .querySelector(".dialog-off-canvas-main-canvas")
-          .classList.remove("noscroll");
-        document.querySelector("#overlay-search").classList.add("search-btn");
-      } else {
-        y.style.height = "100%";
-        y.style.bottom = "0";
-        if (x.style.height == "100%") {
-          x.style.height = "1px";
-          x.style.bottom = "-1px";
-          document.querySelector("#mob-overlay-search").classList.remove("change");
-          document.querySelector("#mob-overlay-search").classList.add("overlay-btn");
-        } else if (s.style.height == "100%") {
-          s.style.height = "1px";
-          s.style.bottom = "-1px";
-          document.querySelector("#overlay-share").classList.remove("change");
-          document.querySelector("#overlay-share").classList.add("share-btn");
-        }
-        document.querySelector("html").classList.add("noscroll");
-        document.querySelector("body").classList.add("noscroll");
-        document.querySelector(".dialog-off-canvas-main-canvas").classList.add("noscroll");
-        document.querySelector("#overlay-search").classList.remove("search-btn");
-      }
-  });
-
-  var tabletOverlayBtn = document.querySelector("#tablet-overlay-btn");
-  if (tabletOverlayBtn != null) {
-    document
-      .querySelector("#tablet-overlay-btn")
-      .addEventListener("click", function navBtn() {
-        document.getElementById("tablet-overlay-btn").classList.toggle("change");
+  if(!frontPage){
+    overlayNavBtn.addEventListener("click", function navBtn() {
+      overlayNavBtn.classList.toggle("change");
         if (x.style.height === "100%") {
-          x.style.height = "0%";
-          document.querySelector("body").classList.remove("noscroll");
-          document.querySelector("#overlay-btn").classList.add("overlay-btn");
-          document.querySelector(".navigation_col").classList.remove("overlay-menu");
+          x.style.height = "1px";
+          x.style.bottom = "-1px";
+          html.classList.remove("noscroll");
+          body.classList.remove("noscroll");
+          dialogOffCanvas.classList.remove("noscroll");
+          overlayNavBtn.classList.add("overlay-btn");
         } else {
           x.style.height = "100%";
-          document.querySelector("body").classList.add("noscroll");
-          document.querySelector("#overlay-btn").classList.remove("overlay-btn");
-          document.querySelector(".navigation_col").classList.add("overlay-menu");
+          x.style.bottom = "0";
+          if (y.style.height == "100%") {
+            y.style.height = "1px";
+            y.style.bottom = "-1px";
+            overlaySearchBtn.classList.remove("change");
+            overlaySearchBtn.classList.add("search-btn");
+          } else if (s.style.height == "100%") {
+            s.style.height = "1px";
+            s.style.bottom = "-1px";
+            overlayShareBtn.classList.remove("change");
+            overlayShareBtn.classList.add("share-btn");
+          }
+          html.classList.add("noscroll");
+          body.classList.add("noscroll");
+          dialogOffCanvas.classList.add("noscroll");
+          overlayNavBtn.classList.remove("overlay-btn");
         }
       });
-  } else {
-    return false;
+
+    overlaySearchBtn.addEventListener("click", function searchBtn() {
+        overlaySearchBtn.classList.toggle("change");
+        if (y.style.height === "100%") {
+          y.style.height = "1px";
+          y.style.bottom = "-1px";
+          html.classList.remove("noscroll");
+          body.classList.remove("noscroll");
+          dialogOffCanvas.classList.remove("noscroll");
+          overlaySearchBtn.classList.add("search-btn");
+          document.querySelector('.overlay-btn').classList.remove("normText");
+        } else {
+          y.style.height = "100%";
+          y.style.bottom = "0";
+          if (x.style.height == "100%") {
+            x.style.height = "1px";
+            x.style.bottom = "-1px";
+            overlaySearchBtn.classList.remove("change");
+            overlaySearchBtn.classList.add("overlay-btn");
+          } else if (s.style.height == "100%") {
+            s.style.height = "1px";
+            s.style.bottom = "-1px";
+            overlayShareBtn.classList.remove("change");
+            overlayShareBtn.classList.add("share-btn");
+          }
+          html.classList.add("noscroll");
+          body.classList.add("noscroll");
+          dialogOffCanvas.classList.add("noscroll");
+          overlaySearchBtn.classList.remove("search-btn");
+          document.querySelector('.overlay-btn').classList.add("normText");
+        }
+      });
+
+    overlayShareBtn.addEventListener("click", function shareBtn() {
+        overlayShareBtn.classList.toggle("change");
+        if (s.style.height === "100%") {
+          s.style.height = "1px";
+          s.style.bottom = "-1px";
+          html.classList.remove("noscroll");
+          body.classList.remove("noscroll");
+          dialogOffCanvas.classList.remove("noscroll");
+          overlayShareBtn.classList.add("share-btn");
+        } else {
+          s.style.height = "100%";
+          s.style.bottom = "0";
+          if (y.style.height == "100%") {
+            y.style.height = "1px";
+            y.style.bottom = "-1px";
+            overlaySearchBtn.classList.remove("change");
+            overlaySearchBtn.classList.add("search-btn");
+          } else if (s.style.height == "100%") {
+            x.style.height = "1px";
+            x.style.bottom = "-1px";
+            overlayNavBtn.classList.remove("change");
+            overlayNavBtn.classList.add("overlay-btn");
+          }
+          html.classList.add("noscroll");
+          body.classList.add("noscroll");
+          dialogOffCanvas.classList.add("noscroll");
+          overlayShareBtn.classList.remove("share-btn");
+        }
+    });
+
+    document.querySelector("#footer-overlay-btn").addEventListener("click", function footerNavBtn() {
+      document.getElementById("footer-overlay-btn").classList.toggle("change");
+        if (x.style.height === "100%") {
+          x.style.height = "1px";
+          x.style.bottom = "-1px";
+          html.classList.remove("noscroll");
+          body.classList.remove("noscroll");
+          dialogOffCanvas.classList.remove("noscroll");
+          overlayNavBtn.classList.add("overlay-btn");
+        } else {
+          x.style.height = "100%";
+          x.style.bottom = "0";
+          if (y.style.height == "100%") {
+            y.style.height = "1px";
+            y.style.bottom = "-1px";
+            overlaySearchBtn.classList.remove("change");
+            overlaySearchBtn.classList.add("search-btn");
+          } else if (s.style.height == "100%") {
+            s.style.height = "1px";
+            s.style.bottom = "-1px";
+            overlayShareBtn.classList.remove("change");
+            overlayShareBtn.classList.add("share-btn");
+          }
+          html.classList.add("noscroll");
+          body.classList.add("noscroll");
+          dialogOffCanvas.classList.add("noscroll");
+          overlayNavBtn.classList.remove("overlay-btn");
+        }
+    });
+
+    document.querySelector("[data-bs-target='#mobile-overlay']").addEventListener("click", function mobNavBtn() {
+      document.querySelector("[data-bs-target='#mobile-overlay']").classList.toggle("change");
+        if (x.style.height === "100%") {
+          x.style.height = "1px";
+          x.style.bottom = "-1px";
+          html.classList.remove("noscroll");
+          body.classList.remove("noscroll");
+          dialogOffCanvas.classList.remove("noscroll");
+        } else {
+          x.style.height = "100%";
+          x.style.bottom = "0";
+          if (y.style.height == "100%") {
+            y.style.height = "1px";
+            y.style.bottom = "-1px";
+            overlaySearchBtn.classList.remove("change");
+            overlaySearchBtn.classList.add("search-btn");
+          } else if (s.style.height == "100%") {
+            s.style.height = "1px";
+            s.style.bottom = "-1px";
+            overlayShareBtn.classList.remove("change");
+            overlayShareBtn.classList.add("share-btn");
+          }
+          html.classList.add("noscroll");
+          body.classList.add("noscroll");
+          dialogOffCanvas.classList.add("noscroll");
+          document.querySelector("[data-bs-target='#mobile-overlay']").classList.remove("overlay-btn");
+        }
+      });
+
+    document.querySelector("#mob-overlay-search").addEventListener("click", function mobSearchBtn() {
+      document.getElementById("mob-overlay-search").classList.toggle("change");
+        if (y.style.height === "100%") {
+          y.style.height = "1px";
+          y.style.bottom = "-1px";
+          html.classList.remove("noscroll");
+          body.classList.remove("noscroll");
+          dialogOffCanvas.classList.remove("noscroll");
+          overlaySearchBtn.classList.add("search-btn");
+        } else {
+          y.style.height = "100%";
+          y.style.bottom = "0";
+          if (x.style.height == "100%") {
+            x.style.height = "1px";
+            x.style.bottom = "-1px";
+            document.querySelector("#mob-overlay-search").classList.remove("change");
+            document.querySelector("#mob-overlay-search").classList.add("overlay-btn");
+          } else if (s.style.height == "100%") {
+            s.style.height = "1px";
+            s.style.bottom = "-1px";
+            overlayShareBtn.classList.remove("change");
+            overlayShareBtn.classList.add("share-btn");
+          }
+          html.classList.add("noscroll");
+          body.classList.add("noscroll");
+          dialogOffCanvas.classList.add("noscroll");
+          overlaySearchBtn.classList.remove("search-btn");
+        }
+    });
   }
 });
