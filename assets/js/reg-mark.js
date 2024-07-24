b7cedd539359e8217f440543656b8b3c267a7e43
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-07-24 09:45:31 +0100

    Testing a very basic version to see the reult

--- assets/js/reg-mark.js
+++ assets/js/reg-mark.js
@@ -33,11 +33,11 @@ document.addEventListener('DOMContentLoaded', function(){
       parent.removeChild(element);
     }
 
+    var pageTitle = document.querySelector(".page-title");
     function replaceTitle() {
-      var pageTitle = document.querySelector(".page-title");
-      if(pageTitle.innerHTML == "Resources"){
+      
         pageTitle.innerHTML = "ESG & Resoruces"
-      }
+      
     }
 
     replaceTitle();
