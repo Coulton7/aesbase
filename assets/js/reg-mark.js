99c7f8581b18cae96c9d1bd4a032c691d7f6b3b3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-07-24 10:02:44 +0100

    Removing console.log though need to find fix for those page titles without spans

--- assets/js/reg-mark.js
+++ assets/js/reg-mark.js
@@ -35,7 +35,6 @@ document.addEventListener('DOMContentLoaded', function(){
 
     var pageTitle = document.querySelector(".page-title h1 span");
     function replaceTitle() {
-      console.log(pageTitle)
       if(pageTitle.innerHTML == "Resources"){
         pageTitle.innerHTML = "ESG & Resoruces"
       } else {
