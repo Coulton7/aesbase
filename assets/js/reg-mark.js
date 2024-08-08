d1e022b1dcb8eafaf2b7460c399829fd53d64066
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-08-08 08:58:46 +0100

    Re-formatted the js

--- assets/js/reg-mark.js
+++ assets/js/reg-mark.js
@@ -1,47 +1,47 @@
 document.addEventListener('DOMContentLoaded', function(){
-    function recurse(element) {
-      if (element.childNodes.length > 0) {
-        for (var i = 0; i < element.childNodes.length; i++) {
-          recurse(element.childNodes[i]);
-        }
-      }
-      if (element.nodeType == 3 && /\S/.test(element.nodeValue)) {
-        doReplacements(element, element.parentElement);
+  function recurse(element) {
+    if (element.childNodes.length > 0) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        recurse(element.childNodes[i]);
       }
     }
-    var html = document.getElementsByTagName('body')[0];
-    recurse(html);
-    function doReplacements(element, parent) {
-      var html = element.data;
-      if (element.nextSibling) {
-        if (element.nextSibling.nodeName.toLowerCase() === 'sup') {
-          return;
-        }
-      }
-      html = element.data.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>");
-      html = html.replace(/<sup>&reg;<\/sup>®/gi, "<sup>&reg;</sup>");
-      var frag = (function() {
-        var wrap = document.createElement('div'),
-          frag = document.createDocumentFragment();
-        wrap.innerHTML = html;
-        while (wrap.firstChild) {
-          frag.appendChild(wrap.firstChild);
-        }
-        return frag;
-      })();
-      parent.insertBefore(frag, element);
-      parent.removeChild(element);
+    if (element.nodeType == 3 && /\S/.test(element.nodeValue)) {
+      doReplacements(element, element.parentElement);
     }
-
-    var pageTitle = document.querySelector(".page-title h1");
-    function replaceTitle() {
-        if(pageTitle.innerText == "Resources"){
-          pageTitle.innerText = "ESG & Resources"
-        } else {
-          return true
-        }
+  }
+  var html = document.getElementsByTagName('body')[0];
+  recurse(html);
+  function doReplacements(element, parent) {
+    var html = element.data;
+    if (element.nextSibling) {
+      if (element.nextSibling.nodeName.toLowerCase() === 'sup') {
+        return;
+      }
+    }
+    html = element.data.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>");
+    html = html.replace(/<sup>&reg;<\/sup>®/gi, "<sup>&reg;</sup>");
+    var frag = (function() {
+      var wrap = document.createElement('div'),
+        frag = document.createDocumentFragment();
+      wrap.innerHTML = html;
+      while (wrap.firstChild) {
+        frag.appendChild(wrap.firstChild);
+      }
+      return frag;
+    })();
+    parent.insertBefore(frag, element);
+    parent.removeChild(element);
   }
 
-    replaceTitle();
+  var pageTitle = document.querySelector(".page-title h1");
+  function replaceTitle() {
+      if(pageTitle.innerText == "Resources"){
+        pageTitle.innerText = "ESG & Resources"
+      } else {
+        return true
+      }
+  }
 
-  });
+  replaceTitle();
+
+});
