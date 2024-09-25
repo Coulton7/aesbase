1a2d410bd27ecfc783bad485b984086655dc5d4d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-09-25 12:18:45 +0100

    Testing a theroy out ot see the result

--- assets/js/build-code.js
+++ assets/js/build-code.js
@@ -124,14 +124,14 @@ function connectAll() {
 }
 document.addEventListener("DOMContentLoaded", function() {
     // reset svg each time 
-    document.querySelector("#svg1").style.height="0";
-    document.querySelector("#svg1").style.width="0";
+    document.querySelector("#svg1").setAttribute("height", 0)
+    document.querySelector("#svg1").setAttribute("width", 0)
     connectAll();
 });
 window.addEventListener("resize", function () {
     // reset svg each time 
     offset = 0;
-    document.querySelector("#svg1").style.height="0";
-    document.querySelector("#svg1").style.width="0";
+    document.querySelector("#svg1").setAttribute("height", 0)
+    document.querySelector("#svg1").setAttribute("width", 0)
     connectAll();
 });
