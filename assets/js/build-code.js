20096605c4786014071bc92b74d41342f69c8dc1
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-10-18 14:43:37 +0100

    Chane the startY over to something else

--- assets/js/build-code.js
+++ assets/js/build-code.js
@@ -22,7 +22,7 @@ function drawPath(svg, path, startX, startY, endX, endY) {
     // get the path's stroke width (if one wanted to be  really precize, one could use half the stroke size)
     var stroke =  parseFloat(path.getAttribute("stroke-width"));
     // check if the svg is big enough to draw the path, if not, set heigh/width
-    if (svg.getAttribute("height") <  endY)                 svg.setAttribute("height", startY);
+    if (svg.getAttribute("height") <  StartY)                 svg.setAttribute("height", startY);
     if (svg.getAttribute("width" ) < (startX + stroke) )    svg.setAttribute("width", (startX + stroke));
     if (svg.getAttribute("width" ) < (endX   + stroke) )    svg.setAttribute("width", (endX   + stroke));
     
