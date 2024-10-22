980bdb1be5b04009ec44a86755e20cf32b3a25d1
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-10-22 16:30:55 +0100

    Testing a theroy

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -789,7 +789,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     transformItems(items){
                         return items.map(item => ({
                             ...item,
-                            label: typeMapping[item.label],
+                            label: typeMapping[item.type],
                         }));
                     },
                     cssClasses: {
