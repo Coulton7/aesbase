471104269162ee01d1dc15511d76f8c9bb674c5c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-10-22 14:49:12 +0100

    Trying to add type/cat to the result

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -783,6 +783,8 @@ document.addEventListener("DOMContentLoaded", function() {
                             <small>https://www.aesseal.com${data.url}</small>
                             <p class="h3 ${data.title ? '' : 'd-none'}">${data.title}</p>
                             <p class="h3 ${data.name_1 ? '' : 'd-none'}">${data.name_1}</p>
+                            <p id="contentCat" class="lead ${data.type ? '' : 'd-none'}">${data.type}</p>
+                            <p id="vocabCat" class="lead ${data.vid ? '' : 'd-none'}">${data.vid}</p>
                             <p class=${data.description ? '' : 'd-none'}>${instantsearch.snippet({
                                 attribute: "description",
                                 hit: data
