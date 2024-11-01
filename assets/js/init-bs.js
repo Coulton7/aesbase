b6ff35f8cb9658582758f32dc85f8c323d9e6f13
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-11-01 16:15:09 +0000

    Adding new js for tbas tosee the result

--- assets/js/init-bs.js
+++ assets/js/init-bs.js
@@ -1,2 +1,8 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const tabEl = document.querySelector('button[data-bs-toggle="tab"]')
+tabEl.addEventListener('shown.bs.tab', event => {
+  event.target 
+  event.relatedTarget
+})
