35d3aacbf3451d5124d7ba1776511388d2633a8b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-11-01 16:18:11 +0000

    REset broke the code

--- assets/js/init-bs.js
+++ assets/js/init-bs.js
@@ -1,8 +1,3 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-const tabEl = document.querySelector('button[data-bs-toggle="tab"]')
-tabEl.addEventListener('shown.bs.tab', event => {
-  event.target 
-  event.relatedTarget
-})
