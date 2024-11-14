62bdf1479da1ffae0705f54a67d6827cf2aaa174
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-11-14 09:59:34 +0000

    Removing code as it breaks the site

--- assets/js/init-bs.js
+++ assets/js/init-bs.js
@@ -1,6 +1,5 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-const triggerEl = document.querySelector('#global-overlay-search[data-bs-target="#globalSearch"')
-bootstrap.Tab.getInstance(triggerEl).show();
+
 
