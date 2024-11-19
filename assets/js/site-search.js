bd9203f6bdd021cc0ffe8b9e3f08eddfcb33962d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-11-19 13:11:29 +0000

    Re-adding original seems to have an issue with the languages

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -911,22 +911,6 @@ document.addEventListener("DOMContentLoaded", function() {
         })
 
         deSearch.addWidgets([
-            {
-                init: function(options) {
-                    if(filterLang == "en")
-                    {
-                        options.helper.toggleRefinement('search_api_language', 'en');
-                    }
-                    else if(filterLang == "")
-                    {
-                        options.helper.toggleRefinement('search_api_language', 'en');
-                    }
-                    else if (filterLang === "de") {
-                        options.helper.toggleRefinement('search_api_language', 'de');
-                    }
-                }
-            },
-            
             instantsearch.widgets.configure({
                 hitsPerPage: 10,
                 attributesToSnippet: ['body:80'],
@@ -1034,7 +1018,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
             instantsearch.widgets
                 .index({ indexName: 'aesseal' })
-                .addWidgets([
+                .addWidgets([{
+                    init: function(options) {
+                        if(filterLang == "en")
+                        {
+                            options.helper.toggleRefinement('search_api_language', 'en');
+                        }
+                        else if(filterLang == "")
+                        {
+                            options.helper.toggleRefinement('search_api_language', 'en');
+                        }
+                        else if (filterLang === "de") {
+                            options.helper.toggleRefinement('search_api_language', 'de');
+                        }
+                    }
+                },
 
                 instantsearch.widgets.configure({
                     hitsPerPage: 10,
