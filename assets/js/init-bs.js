87b5af335d34fb184844a5efca5354bce6c9156a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-11-19 16:17:12 +0000

    Adding a bit of js for testing

--- assets/js/init-bs.js
+++ assets/js/init-bs.js
@@ -1,5 +1,9 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-
-
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#global-overlay-search").addEventListener('click', function(){
+        var resouceSearch = document.querySelector('#resourceSearch')
+        bootstrap.Tab.getOrCreateInstance(resouceSearch).show
+    })
+});
