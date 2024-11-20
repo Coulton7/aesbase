695cbcd8ef53ee96dd2d319e5c49783111d33061
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-11-20 09:59:46 +0000

    Adding if statement to stop js from looking for value if not on page

--- assets/js/init-bs.js
+++ assets/js/init-bs.js
@@ -2,8 +2,11 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("#global-overlay-search").addEventListener('click', function(){
-        var resouceSearch = document.querySelector('#resourceSearch')
-        bootstrap.Tab.getOrCreateInstance(resouceSearch).show()
-    })
+    var resourceSearchTriegger = document.querySelector("#global-overlay-search");
+    if(resourceSearchTriegger){
+        resourceSearchTriegger.addEventListener('click', function(){
+            var resouceSearch = document.querySelector('#resourceSearch')
+            bootstrap.Tab.getOrCreateInstance(resouceSearch).show()
+        })
+    }
 });
