3c922d9d9dcd503b9ca5fedb4afa4027842629b0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-11-28 10:11:13 +0000

    Quick reset as a test

--- assets/js/menu-collapse.js
+++ assets/js/menu-collapse.js
@@ -1,33 +1,28 @@
-var overlayBtn = document.getElementById('overlay-btn');
 function autocollapse() {
     var navbar = document.getElementById('autocollapse');
     navbar.classList.remove('collapsed');
-
-    if(overlayBtn){
-      if(navbar.innerHeight > 100) {
-        navbar.classList.add('collapsed');
-        overlayBtn.classList.add('display-none-important');
-      } else {
-        navbar.classList.remove('collapsed');
-        overlayBtn.classList.remove('display-none-important');
-      }
+    if(navbar.innerHeight > 100) {
+      navbar.classList.add('collapsed');
+      document.getElementById('overlay-btn').classList.add('display-none-important');
+    } else {
+      navbar.classList.remove('collapsed');
+      document.getElementById('overlay-btn').classList.remove('display-none-important');
     }
   }
   
   function menuItemOverlay() {
-    if (overlayBtn){
-      if(window.innerWidth > 992) {
-        let overlayBtn = document.querySelector('.overlay-btn').getBoundingClientRect();
-        let overlayBtnLeft = overlayBtn.left;
-        let menuItem = document.querySelector('.desktop-last').getBoundingClientRect();
-        let menuItemRight = menuItem.right;
-    
-        if ((menuItemRight > overlayBtnLeft)) {
-          overlayBtn.classList.add('display-none-important');
-        }
-        else {
-          overlayBtn.classList.remove('display-none-important');
-        }
+  
+    if(window.innerWidth > 992) {
+      let overlayBtn = document.querySelector('.overlay-btn').getBoundingClientRect();
+      let overlayBtnLeft = overlayBtn.left;
+      let menuItem = document.querySelector('.desktop-last').getBoundingClientRect();
+      let menuItemRight = menuItem.right;
+  
+      if ((menuItemRight > overlayBtnLeft)) {
+        document.getElementById('overlay-btn').classList.add('display-none-important');
+      }
+      else {
+        document.getElementById('overlay-btn').classList.remove('display-none-important');
       }
     }
   }
@@ -35,21 +30,17 @@ function autocollapse() {
   document.addEventListener("DOMContentLoaded", autocollapse);
   document.addEventListener("DOMContentLoaded", menuItemOverlay);
   window.addEventListener('resize', function() {
-    if(overlayBtn){
-      if(overlayBtn.classList.contains("change")) {
-        return;
-      } else {
-        autocollapse();
-      }
+    if(document.getElementById("overlay-btn").classList.contains("change")) {
+      return;
+    } else {
+      autocollapse();
     }
   });
   window.addEventListener('resize', function() {
-    if(overlayBtn){
-      if(this.overlayBtn.classList.contains("change")) {
-        return;
-      } else {
-        menuItemOverlay();
-      }
+    if(this.document.getElementById("overlay-btn").classList.contains("change")) {
+      return;
+    } else {
+      menuItemOverlay();
     }
   });
   
