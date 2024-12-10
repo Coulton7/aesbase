0048a9c16af3918567c67a234ab62c63a7dde383
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-12-10 10:03:36 +0000

    Changing to flex instead

--- assets/js/init-bs.js
+++ assets/js/init-bs.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if(!nav) {
         document.querySelector('.navbar-toggler').style.display = "none";
-        document.querySelector('.collapse').style.display="block";
+        document.querySelector('.collapse').style.display="flex";
     }
 
 });
