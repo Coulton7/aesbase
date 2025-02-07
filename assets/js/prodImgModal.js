93be28197646872b3ef5e61711741f1a106af6ec
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-07 10:52:47 +0000

    Trying a new js variation

--- assets/js/prodImgModal.js
+++ assets/js/prodImgModal.js
@@ -1,14 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const prodImg = Array.from(document.querySelectorAll(".prod-image-container img"));
     const prodImgModal = document.getElementById('prodImgModal')
 
-    prodImgModal.addEventListener('show.bs.modal', event => {
-        prodImg.forEach(function(event){
-            event.addEventListener("click", function(e){
-                var zoomedImage = document.querySelector(e.relatedTarget).getAttribute('src');
-                console.log(zoomedImage)
-                
-            })
-        })
+    prodImgModal.addEventListener('show.bs.modal', function(event) {
+        document.getElementById('zoomedImage').src = event.relatedTarget.src;
     })
 });
