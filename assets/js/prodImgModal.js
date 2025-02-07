0770d0920242f613d57fb84587335b71a68d1a69
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-07 10:32:43 +0000

    Trying new js variation

--- assets/js/prodImgModal.js
+++ assets/js/prodImgModal.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const prodImg = Array.from(document.querySelectorAll(".prod-image-container img"));
+    const prodImg = document.querySelectorAll(".prod-image-container img");
     const prodImgModal = document.getElementById('prodImgModal')
 
     prodImgModal.addEventListener('show.bs.modal', event => {
-        prodImg.forEach(function(event){
-            event.addEventListener("click", function(){
-                prodImg.forEach( function( ele ){
-                    var image = ele.getAttribute("src")
-                    console.log(image)
-                    document.getElementById("zoomedImage").setAttribute("src", image);
-                })
-            })
+        event.addEventListener("click", function(){
+            for(i=0; i < prodImg.length; i++){
+                prodImg[i].onclick='';
+                document.getElementById("zoomedImage").setAttribute("src", image);
+            }
         })
     })
 });
