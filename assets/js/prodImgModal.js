201f8d78cc7968ba2a0c6ab3cc704fa9f07bf73f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-07 11:07:45 +0000

    Reset

--- assets/js/prodImgModal.js
+++ assets/js/prodImgModal.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const prodImg = Array.from(document.querySelectorAll(".prod-image-container img"));
     const prodImgModal = document.getElementById('prodImgModal')
 
-    prodImgModal.addEventListener('show.bs.modal', function(event) {
-        document.getElementById('zoomedImage').src = event.target.dataset.src;
-        console.log(event.relatedTarget.dataset.src);
+    prodImgModal.addEventListener('show.bs.modal', event => {
+        prodImg.forEach(function(event){
+            event.addEventListener("click", function(e){
+                var zoomedImage = document.querySelector(e.relatedTarget).getAttribute('src');
+                console.log(zoomedImage)
+                
+            })
+        })
     })
 });
