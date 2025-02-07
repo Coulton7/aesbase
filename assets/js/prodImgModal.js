be54d5f397693202b76b5b4fdf2ac5e3a043f8b7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-07 11:25:49 +0000

    Trying another version to see the resutl

--- assets/js/prodImgModal.js
+++ assets/js/prodImgModal.js
@@ -1,18 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     document.addEventListener("DOMContentLoaded", function() {
-        const prodImg = Array.from(document.querySelectorAll(".prod-image-container img"));
         const prodImgModal = document.getElementById('prodImgModal')
     
-        prodImgModal.addEventListener('show.bs.modal', event => {
-            prodImg.forEach(function(event){
-                event.addEventListener("click", function(e){
-                    for(i = 0;i < img.length; i++){
-                        img[i].onclick = '';
-                        console.log(img[i].src);     
-                    }
-                })
-            })
-        })
+        prodImgModal.addEventListener('show.bs.modal', function(event) {
+            document.getElementById('modalImage').src = event.relatedTarget.src;
+            console.log(event.relatedTarget);
+          });
     })
 })
