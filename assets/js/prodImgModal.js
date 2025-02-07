78780e59f9767c364fe54576952b047d90adf404
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-07 12:12:52 +0000

    Trialing a differnet method

--- assets/js/prodImgModal.js
+++ assets/js/prodImgModal.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const prodImgModal = document.getElementById('prodImgModal');
     
     prodImgModal.addEventListener('show.bs.modal', function(event) {
-        document.getElementById('zoomedImage').src = event.relatedTarget.src;
-        console.log(event.querySelector.getAttribute("data-img-url"));
+        document.getElementById('zoomedImage').src = event.relatedTarget.dataset.data-img-url;
+        console.log(event.relatedTarget.dataset.data-img-url);
     });
 })
