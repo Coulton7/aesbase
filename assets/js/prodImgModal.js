e8eca45ab3c7868f606024a1fcaffcb7e15f8844
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-07 10:34:11 +0000

    Trying a small variation

--- assets/js/prodImgModal.js
+++ assets/js/prodImgModal.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const prodImgModal = document.getElementById('prodImgModal')
 
     prodImgModal.addEventListener('show.bs.modal', event => {
-        event.addEventListener("click", function(){
+        document.addEventListener("click", function(){
             for(i=0; i < prodImg.length; i++){
                 prodImg[i].onclick='';
                 document.getElementById("zoomedImage").setAttribute("src", image);
