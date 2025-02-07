5fa1becfc1f8e3df3799afe257a8c23e11ee1779
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-07 11:15:06 +0000

    Small adjustment to the js to get console.log

--- assets/js/prodImgModal.js
+++ assets/js/prodImgModal.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const prodImg = Array.from(document.querySelectorAll(".prod-image-container img"));
-    const prodImgModal = document.getElementById('prodImgModal')
 
-    prodImgModal.addEventListener('show.bs.modal', event => {
-        prodImg.forEach(function(event){
-            event.addEventListener("click", function(){
-                prodImg.forEach( function( event ){
-                    var image = event.getAttribute("src")
-                    document.getElementById("zoomedImage").setAttribute("src", image);
-                    console.log(image)
+    document.addEventListener("DOMContentLoaded", function() {
+        const prodImg = Array.from(document.querySelectorAll(".prod-image-container img"));
+        const prodImgModal = document.getElementById('prodImgModal')
+    
+        prodImgModal.addEventListener('show.bs.modal', event => {
+            prodImg.forEach(function(event){
+                event.addEventListener("click", function(){
+                    prodImg.forEach( function( event ){
+                        var image = event.getAttribute("src")
+                        console.log(image)
+                        document.getElementById("zoomedImage").setAttribute("src", image);
+                    })
                 })
             })
         })
     })
-});
+})
