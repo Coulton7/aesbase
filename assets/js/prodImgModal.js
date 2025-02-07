e02657905b1988a6ac4a08d42c304728203428df
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-07 10:44:00 +0000

    Reset back to originial

--- assets/js/prodImgModal.js
+++ assets/js/prodImgModal.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const prodImg = document.querySelectorAll(".prod-image-container img");
+    const prodImg = Array.from(document.querySelectorAll(".prod-image-container img"));
     const prodImgModal = document.getElementById('prodImgModal')
 
     prodImgModal.addEventListener('show.bs.modal', event => {
-        document.addEventListener("click", function(){
-            for(i=0; i < prodImg.length; i++){
-                prodImg[i].onclick='';
-                document.getElementById("zoomedImage").setAttribute("src", image);
-            }
+        prodImg.forEach(function(event){
+            event.addEventListener("click", function(){
+                prodImg.forEach( function( event ){
+                    var image = event.getAttribute("src")
+                    console.log(image);
+                    document.getElementById("zoomedImage").setAttribute("src", image);
+                })
+            })
         })
     })
 });
