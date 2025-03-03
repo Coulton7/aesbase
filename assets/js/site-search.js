c17b5701034068272e9bfa7279c79393f10a70f3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-03-03 14:58:27 +0000

    Seeing what happens with indices

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1012,7 +1012,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
                         const queryString = qsModule.stringify(queryParameters, {
                             addQueryPrefix: true,
-                            arrayFormat: 'repeat'
+                            arrayFormat: 'indices'
                         });
 
                         return `${origin}${pathname}${queryString}${hash}`;
