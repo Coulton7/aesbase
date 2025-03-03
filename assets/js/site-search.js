2549dec4d49fbcca454c616dceada44e25a272d2
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-03-03 09:49:02 +0000

    REset back to basic for testing

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1005,50 +1005,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             },
             routing: {
-                router: instantsearch.routers.history({
-
-                    createURL({ qsModule, routeState, location }) {
-                        const { origin, pathname, hash} = location;
-                        const queryParameters = {};
-
-                        if(routeState.q) {
-                            queryParameters.q = encodeURIComponent(routeState.q);
-                        }
-                        if(routeState.type) {
-                            queryParameters.type = routeState.type.map(encodeURIComponent);
-                        }
-                        if(routeState.lang) {
-                            queryParameters.lang = routeState.lang.map(encodeURIComponent);
-                        }
-
-                        const queryString = qsModule.stringify(queryParameters, {
-                            addQueryPrefix: true,
-                            arrayFormat: 'repeat'
-                        });
-
-                        return `${origin}${pathname}${queryString}${hash}`;
-                    },
-
-                    parseUrl({ qsModule, location }) {
-                        const { q = '', type = [], lang =[] } = qsModule.parse(
-                            location.search.slice(1)
-                        );
-                        const allType = Array.isArray(type)
-                            ? type
-                            : [type].filter(Boolean);
-                        const allLang = Array.isArray(lang)
-                            ? lang
-                            :[lang].filter(Boolean);
-                        return {
-                            q: decodeURIComponent(q),
-                            type: allType.map(decodeURIComponent),
-                            lang: allLang.map(decodeURIComponent)
-                        };
-                        
-                    },
-                    writeDelay: 400,
-                    cleanUrlOnDispose: true,
-                }),
+                router: {
                 stateMapping: {
                     stateToRoute(uiState){
                         const indexUiState = uiState['aesseal'] || {};
@@ -1071,6 +1028,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     },
                 },
             },
+        },
         });
 
         search.addWidgets([{
