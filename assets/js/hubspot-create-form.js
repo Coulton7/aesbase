8177953d918f01de6770cff228cf108e7c410269
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-03-27 13:22:31 +0000

    Adding spanish api plan form to the js

--- assets/js/hubspot-create-form.js
+++ assets/js/hubspot-create-form.js
@@ -250,6 +250,44 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
                 }
             });
+        } else if (document.querySelector("#piping-booklet-form")){
+            document.querySelector('#block-aesbase-resourcesubmissionnotice').style.display = 'none';
+            if (document.querySelector('.resource-request').innerHTML.length > 0) {
+                document.querySelector('.pdf-download').style.display = 'none';
+            }
+            
+            function getUrlVars() {
+                var vars = [], hash;
+                var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+                for(var i = 0; i < hashes.length; i++) {
+                    hash = hashes[i].split('=');
+                    vars.push(hash[0]);
+                    vars[hash[0]] = hash[1];
+                }
+                return vars;
+            }
+            
+            var submitString = getUrlVars()["fs"];
+            if(submitString=='y'){
+                document.querySelector('.pdf-download').style.display = 'block';;
+                document.querySelector('.resource-request').style.display = 'none';
+                document.querySelector('#block-aesbase-resourcesubmissionnotice').style.display = 'block';;
+            }
+
+            hbspt.forms.create({
+                portalId: "2248916",
+                formId: "0d6eb558-5d3a-48e6-977f-448765cc24b6",
+                target: "#piping-booklet-form",
+                css: "",
+                cssClass:"hs-overlay-form",
+                submitButtonClass:"btn btn-primary hs-button",
+                onFormSubmit: function ($form){
+                    var deliverability = parseFloat (document.querySelector('.deliverable').textContent);
+                    if(deliverability === 0){
+                        window.location.href = window.location.pathname+'?fs=y'
+                    }
+                }
+            });
         }
     } else if(filterLang =='fr'){
         if(document.querySelector("#contact-form")){
