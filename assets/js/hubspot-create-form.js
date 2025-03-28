bfd32960f62030c46763a4a0f15d137f722e7c54
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-03-28 10:31:43 +0000

    Full reset test on anothe video to see the outcome

--- assets/js/hubspot-create-form.js
+++ assets/js/hubspot-create-form.js
@@ -91,6 +91,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             });
         } else if (document.querySelector("#video-request")) {
+                document.querySelector('.video-thumbnail').style.display = 'none';
+                if (document.querySelector('.resource-request').innerHTML.length > 0) {
+                  document.querySelector('.video-stream').style.display = 'none';
+                  document.querySelector('.video-thumbnail').style.display = 'block';
+                }
+            
+                function getUrlVars() {
+                  var vars = [], hash;
+                  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+                  for(var i = 0; i < hashes.length; i++) {
+                    hash = hashes[i].split('=');
+                    vars.push(hash[0]);
+                    vars[hash[0]] = hash[1];
+                  }
+                  return vars;
+                }
+            
+                var submitString = getUrlVars()["fs"];
+                if(submitString=='y'){
+                  document.querySelector('.video-stream').style.display = 'block';;
+                  document.querySelector('.resource-request').style.display = 'none';
+                  document.querySelector('.video-thumbnail').style.display = 'none';
+                }
+
                 hbspt.forms.create({
                     portalId: "2248916",
                     formId: "0d6eb558-5d3a-48e6-977f-448765cc24b6",
