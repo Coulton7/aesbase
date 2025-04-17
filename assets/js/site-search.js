d1434092d252ce8c3f2c58ee3232e8f20ee3f400
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-04-17 15:06:07 +0100

    Trying a new code variation to see the result

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1489,12 +1489,21 @@ document.addEventListener("DOMContentLoaded", function() {
                     },
                 },
                 transformItems(items, { results }){
-                    if(results.query === '') return [];
-                    return items.map(item => ({
-                        ...item,
-                        type: typeMapping[item.type],
-                        vid: vidMapping[item.vid]
-                    }))
+                    if(window.location.search.includes('type=')){
+                        return items.map(item => ({
+                            ...item,
+                            type: typeMapping[item.type],
+                            vid: vidMapping[item.vid]
+                        }))
+                    } else {
+                        if(results.query === '') return [];
+                        return items.map(item => ({
+                            ...item,
+                            type: typeMapping[item.type],
+                            vid: vidMapping[item.vid]
+                        }))
+                    }
+                    
                                     }
             }),
         ]);
