c91b63cbcfa3d29df76195e91e2a82235e363dc4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-04-23 10:33:18 +0100

    Trying a new piece of code to see the result

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1119,44 +1119,45 @@ document.addEventListener("DOMContentLoaded", function() {
 
         search.addWidgets([{
             init: function(options) {
-                console.log(filterLang);
-                if(filterLang == "en")
-                {
-                    options.helper.toggleRefinement('search_api_language', 'en');
-                }
-                else if(filterLang == "")
-                {
-                    options.helper.toggleRefinement('search_api_language', 'en');
-                }
-                else if(filterLang == "es")
-                {
-                    options.helper.toggleRefinement('search_api_language', 'es');
-                }
-                else if (filterLang === "fr") {
-                    options.helper.toggleRefinement('search_api_language', 'fr');
-                }
-                else if (filterLang === "de") {
-                    options.helper.toggleRefinement('search_api_language', 'de');
-                }
-                else if(filterLang == "it")
-                {
-                    options.helper.toggleRefinement('search_api_language', 'it');
-                }
-                else if(filterLang == "pl")
-                {
-                    options.helper.toggleRefinement('search_api_language', 'pl');
-                }
-                else if(filterLang == "ru")
-                {
-                    options.helper.toggleRefinement('search_api_language', 'ru');
-                }
-                else if(filterLang == "tr")
-                {
-                    options.helper.toggleRefinement('search_api_language', 'tr');
-                }
-                else if(filterLang == "zh-hans")
-                {
-                    options.helper.toggleRefinement('search_api_language', 'zh-hans');
+                if(document.location.href.indexOf('lang[') === -1){
+                    if(filterLang == "en")
+                    {
+                        options.helper.toggleRefinement('search_api_language', 'en');
+                    }
+                    else if(filterLang == "")
+                    {
+                        options.helper.toggleRefinement('search_api_language', 'en');
+                    }
+                    else if(filterLang == "es")
+                    {
+                        options.helper.toggleRefinement('search_api_language', 'es');
+                    }
+                    else if (filterLang === "fr") {
+                        options.helper.toggleRefinement('search_api_language', 'fr');
+                    }
+                    else if (filterLang === "de") {
+                        options.helper.toggleRefinement('search_api_language', 'de');
+                    }
+                    else if(filterLang == "it")
+                    {
+                        options.helper.toggleRefinement('search_api_language', 'it');
+                    }
+                    else if(filterLang == "pl")
+                    {
+                        options.helper.toggleRefinement('search_api_language', 'pl');
+                    }
+                    else if(filterLang == "ru")
+                    {
+                        options.helper.toggleRefinement('search_api_language', 'ru');
+                    }
+                    else if(filterLang == "tr")
+                    {
+                        options.helper.toggleRefinement('search_api_language', 'tr');
+                    }
+                    else if(filterLang == "zh-hans")
+                    {
+                        options.helper.toggleRefinement('search_api_language', 'zh-hans');
+                    }
                 }
             }
         }]);
