846bff489fb74c85f47a2a5de193398168e0c96a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-04-23 09:51:36 +0100

    Trialing a new setting on the refinement list to see the result

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1108,8 +1108,10 @@ document.addEventListener("DOMContentLoaded", function() {
                                 page:routeState.page,
                                 refinementList: {
                                     type: routeState.type,
+                                },
+                                refinementList: {
                                     search_api_language: routeState.lang
-                                }
+                                },
                             },
                         };
                     },
