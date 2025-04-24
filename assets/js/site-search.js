80e6cd4cfee5fcfcb857354464d5bf8209b88e28
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-04-24 12:02:16 +0100

    Adding more settings to stop initial search load

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -5472,6 +5472,24 @@ document.addEventListener("DOMContentLoaded", function() {
             indexName: 'aesseal',
             typoTolerance: 'strict',
             paginationLimitedTo: 80,
+            search(requests) {
+                if(requests.every(({ params }) => !params.query)) {
+                    return Promise.resolve({
+                        results: requests.map(() => ({
+                            hits: [],
+                            nbHits: 0,
+                            nbPages: 0,
+                            page: 0,
+                            hitsPerPage: 0,
+                            processingTimeMS: 0,
+                            widgetParams: 0,
+                            query: '',
+                            params: '',
+                        })),
+                    });
+                }
+                return searchClient.search(requests);
+            },
             insights: {
                 onEvent(event) {
                     const { widgetType, eventType, payload, hits } = event;
