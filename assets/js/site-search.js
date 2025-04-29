a34505d2dcd960c9a4329b89f23e64fa47bba4db
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-04-29 12:11:38 +0100

    Trying .includes instead

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -816,7 +816,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         const langlistPanel = instantsearch.widgets.panel ({
             hidden: function(options) {
-                if (window.location.href.indexOf('?q=') === -1){
+                if (window.location.href.includes('?q=') === -1){
                     console.log(window.location.href)
                     return [];
                 } else {
