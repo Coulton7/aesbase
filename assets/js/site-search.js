3e9d458a896f4e0e78ad85239c768c4b10e759dc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-06-19 16:04:53 +0100

    Resetting to an older search helper to prevent empty searches occuring at all

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1100,14 +1100,12 @@ document.addEventListener("DOMContentLoaded", function() {
             indexName: 'aesseal',
             typoTolerance: 'strict',
             paginationLimitedTo: 80,
-            search(requests) {
-                const newRequests = requests.map((request) => {
-                    if(!request.params.query || request.params.query.length === 0) {
-                        request.params.analytics = false;
-                    }
-                    return request;
-                });
-                return algoliaClient.search(newRequests);
+            searchFunction(helper) {
+                if (helper.state.query === '')
+                {
+                    return;
+                }
+                helper.search();
             },
             
             insights: {
