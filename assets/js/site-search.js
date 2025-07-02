4192b7f6491e3adc7633c813060152bb7ef8c4a2
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-07-02 15:10:21 +0100

    Testing with repeat like the example to see the result

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1139,7 +1139,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
                         const queryString = qsModule.stringify(queryParameters, {
                             addQueryPrefix: true,
-                            arrayFormat: 'indices',
+                            arrayFormat: 'repeat',
                         });
 
                         return `${origin}${pathname}${queryString}${hash}`;
