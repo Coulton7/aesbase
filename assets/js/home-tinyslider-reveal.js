7b42c2fb7e086935993d7063acb12ffa9645ba80
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-07-17 11:31:56 +0100

    New js controls for the new sliders

--- assets/js/home-tinyslider-reveal.js
+++ assets/js/home-tinyslider-reveal.js
@@ -8,11 +8,9 @@ document.addEventListener("DOMContentLoaded", function() {
             document.querySelector("#pillarTab").classList.add('d-md-block', 'd-inline-flex');
             document.querySelector("#newsBtn").classList.remove('invisible');
             document.querySelector("#newsBtn").classList.add('visible')
-            document.querySelector(".news-slider").classList.add('d-none');
+            document.querySelector(".latest-news-slider").classList.add('d-none');
             document.querySelector(".pillar-slider").classList.remove('d-none');
-            document.querySelector("#pillarCounter").classList.remove('d-none');
             document.querySelector("#pillar-controls").classList.remove('d-none');
-            document.querySelector("#newsCounter").classList.add('d-none');
             document.querySelector("#news-controls").classList.add('d-none');
             document.querySelector('.pillar-btns').classList.add('visible');
             document.querySelector('.pillar-btns').classList.remove('invisible');
@@ -27,10 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
             document.querySelector("#newsBtn").classList.add('invisible');
             document.querySelector("#newsBtn").classList.remove('visible');
             document.querySelector(".pillar-slider").classList.add('d-none');
-            document.querySelector(".news-slider").classList.remove('d-none');
-            document.querySelector("#pillarCounter").classList.add('d-none');
+            document.querySelector(".latest-news-slider").classList.remove('d-none');
             document.querySelector("#pillar-controls").classList.add('d-none');
-            document.querySelector("#newsCounter").classList.remove('d-none');
             document.querySelector("#news-controls").classList.remove('d-none');
             document.querySelector('.pillar-btns').classList.add('invisible');
             document.querySelector('.pillar-btns').classList.remove('visible');
