46a0c573aa2f43b9e6fe88324ed4e4eacc9429ba
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-08-26 12:01:56 +0100

    Updating to match other forms to work on the id

--- assets/js/hubspot-create-form.js
+++ assets/js/hubspot-create-form.js
@@ -119,8 +119,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
                 }
             });
-        } else if (document.querySelector(".video-form")) {
-                console.log('video form');
+        } else if (document.querySelector("#video-form")) {
                 document.querySelector('.video-thumbnail').style.display = 'none';
                 if (document.querySelector('.resource-request').innerHTML.length > 0) {
                     document.querySelector('.video-stream').style.display='none';
