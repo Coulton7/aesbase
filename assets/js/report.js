7972621b0bdcf3ae010519f090274692ae6062b7
Coulton7 <matthew.coulton@aesseal.co.uk>
2025-09-04 11:56:03 +0100

    Adjusting a js to prevent it from running on mobile

--- assets/js/report.js
+++ assets/js/report.js
@@ -4,28 +4,30 @@ document.addEventListener('DOMContentLoaded', function(){
     var overlayLink = document.querySelector('.overlay-btn');
 
     // Ensure carousel element exists before adding event listener
-    var carousel = document.querySelector('.carousel');
-    if (carousel) {
-        carousel.addEventListener('slid.bs.carousel', function () {
-            // Get the currently active carousel item
-            var activeSlide = document.querySelector('.carousel-item.active');
+    if(window.innerWidth > 992) {
+        var carousel = document.querySelector('.carousel');
+        if (carousel) {
+            carousel.addEventListener('slid.bs.carousel', function () {
+                // Get the currently active carousel item
+                var activeSlide = document.querySelector('.carousel-item.active');
 
-            // Check if the active item is the last slide with the id 'homePage2'
-            if (activeSlide && activeSlide.id === 'homePage2') {
-                // Change the font color of the nav links to white
-                navLinks.forEach(function (navLink) {
-                    navLink.style.color = 'white';
-                });
+                // Check if the active item is the last slide with the id 'homePage2'
+                if (activeSlide && activeSlide.id === 'homePage2') {
+                    // Change the font color of the nav links to white
+                    navLinks.forEach(function (navLink) {
+                        navLink.style.color = 'white';
+                    });
 
-                overlayLink.style.color="white";
+                    overlayLink.style.color="white";
 
-            } else {
-                // Reset the font color of the nav links to default or another color
-                navLinks.forEach(function (navLink) {
-                    navLink.style.color = ''; // reset to default
-                });
-            }
-        });
+                } else {
+                    // Reset the font color of the nav links to default or another color
+                    navLinks.forEach(function (navLink) {
+                        navLink.style.color = ''; // reset to default
+                    });
+                }
+            });
+        }
     }
     
 });
