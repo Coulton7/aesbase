101680ed6a36e1ff87892d22567e98e65730a0a4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-09-29 12:47:34 +0100

    Commenting out js that causes error

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -89,44 +89,44 @@ document.addEventListener("DOMContentLoaded", function() {
         widgetParams.container.querySelector('span').hidden = !isSearchStalled;
     };
 
-    const renderPopularSearchBox = (renderOptions, isFirstRender) => {
-        const { query, refine, clear, isSearchStalled, widgetParams } = renderOptions;
+    //const renderPopularSearchBox = (renderOptions, isFirstRender) => {
+      //  const { query, refine, clear, isSearchStalled, widgetParams } = renderOptions;
 
-        if (isFirstRender) {
-            const input = document.createElement('input');
-            input.classList.add('ais-SearchBox-input');
-            input.classList.add('form-control');
-            input.classList.add('pop-search-bar');
-            input.setAttribute("type", "text")
+       // if (isFirstRender) {
+         //   const input = document.createElement('input');
+           // input.classList.add('ais-SearchBox-input');
+           // input.classList.add('form-control');
+           // input.classList.add('pop-search-bar');
+           // input.setAttribute("type", "text")
 
-            const searchButton = document.createElement('button');
-            searchButton.classList.add('ais-SearchBox-submit');
-            searchButton.classList.add('btn');
-            searchButton.classList.add('btn-danger');
-            searchButton.classList.add('pop-search-button');
-            searchButton.innerHTML = '<i class="fa-solid fa-magnifying-glass"></i>';
+           // const searchButton = document.createElement('button');
+           // searchButton.classList.add('ais-SearchBox-submit');
+           // searchButton.classList.add('btn');
+           // searchButton.classList.add('btn-danger');
+           // searchButton.classList.add('pop-search-button');
+           // searchButton.innerHTML = '<i class="fa-solid fa-magnifying-glass"></i>';
 
-            const loadingIndicator = document.createElement('span');
-            loadingIndicator.textContent = 'Loading...';
+           // const loadingIndicator = document.createElement('span');
+           // loadingIndicator.textContent = 'Loading...';
 
-            searchButton.addEventListener('click', event => {
-                refine(input.value);
-            });
+          //  searchButton.addEventListener('click', event => {
+           //     refine(input.value);
+           // });
 
-            input.addEventListener('keydown', function(e){
-                if(e.code === "Enter") {
-                    refine(input.value);
-                }
-            });
+            //input.addEventListener('keydown', function(e){
+               // if(e.code === "Enter") {
+                //    refine(input.value);
+              //  }
+            //});
 
-            widgetParams.container.appendChild(input);
-            widgetParams.container.appendChild(searchButton);
-            widgetParams.container.appendChild(loadingIndicator);
-        }
+           // widgetParams.container.appendChild(input);
+           // widgetParams.container.appendChild(searchButton);
+         //   widgetParams.container.appendChild(loadingIndicator);
+       // }
 
-        widgetParams.container.querySelector('input').value = query;
-        widgetParams.container.querySelector('span').hidden = !isSearchStalled;
-    };
+       // widgetParams.container.querySelector('input').value = query;
+      //  widgetParams.container.querySelector('span').hidden = !isSearchStalled;
+    //};
 
     const renderClearRefinements = (renderOptions, isFirstRender) => {
         const {canRefine, refine, widgetParams} = renderOptions;
@@ -370,9 +370,9 @@ document.addEventListener("DOMContentLoaded", function() {
         renderSearchBox
     );
 
-    const popularSearchBox = connectSearchBox(
-        renderPopularSearchBox
-    );
+    //const popularSearchBox = connectSearchBox(
+      //  renderPopularSearchBox
+    //);
 
     let typeMapping;
     let vidMapping;
@@ -1276,14 +1276,14 @@ document.addEventListener("DOMContentLoaded", function() {
                  searchAsYouType: false,
             }),
 
-            popularSearchBox({
-                container: document.querySelector('#popular-search-bar'),
-                searchAsYouType: false,
-                cssClasses: {
-                    root: 'popular-search',
-                    form: 'pop-search-bar',
-                }
-            }),
+            //popularSearchBox({
+                //container: document.querySelector('#popular-search-bar'),
+                //searchAsYouType: false,
+                //cssClasses: {
+                  //  root: 'popular-search',
+                //    form: 'pop-search-bar',
+              //  }
+            //}),
 
             customStats({
                 container: document.querySelector("#stats"),
