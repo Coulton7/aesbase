3451ba88e8b9064843afd0772c99c2bc7ef6cdc2
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-10-28 12:40:10 +0000

    Adding the correct filter for the search

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1761,7 +1761,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 hitsPerPage: 10,
                 attributesToSnippet: ['body:80'],
                 page: 0,
-                
+                filters: 'NOT field_global_article: "true"',
             }),
 
             typelistPanel({
