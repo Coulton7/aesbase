e358084950b523294033d619903140a95e932ff3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-03-24 15:47:26 +0000

    Adding new if statment to remove an error with js search

--- assets/js/search-tab.js
+++ assets/js/search-tab.js
@@ -1,23 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    const intResultsNum = document.querySelector('#intResultsNum');
-    const globalResultsNum = document.querySelector('#globalResultsNum');
-    var searchButton = document.querySelector('.ais-SearchBox-submit');
-    var searchInput = document.querySelector('.ais-SearchBox-input');
+    let searchTabs = document.querySelector('.search-tabs');
 
-    searchButton.addEventListener("click", function() {
-        setTimeout(function() {
-            if (intResultsNum.innerText === "0" && globalResultsNum.innerText >= "1") {
-                document.querySelector('#globalResultsTab').classList.add('highlight-tab');
-            } else {
-                document.querySelector('#globalResultsTab').classList.remove('highlight-tab');
-            }
-            document.querySelector(".stats-row").style.visibility = "visible";
-        }, 500);
-    });
+    if (!!searchTabs) {
+        const intResultsNum = document.querySelector('#intResultsNum');
+        const globalResultsNum = document.querySelector('#globalResultsNum');
+        var searchButton = document.querySelector('.ais-SearchBox-submit');
+        var searchInput = document.querySelector('.ais-SearchBox-input');
 
-    searchInput.addEventListener("keydown", function(e) {
-        if (e.key === 'Enter') {
+        searchButton.addEventListener("click", function() {
             setTimeout(function() {
                 if (intResultsNum.innerText === "0" && globalResultsNum.innerText >= "1") {
                     document.querySelector('#globalResultsTab').classList.add('highlight-tab');
@@ -26,7 +17,19 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
                 document.querySelector(".stats-row").style.visibility = "visible";
             }, 500);
-        }
-    })
+        });
 
+        searchInput.addEventListener("keydown", function(e) {
+            if (e.key === 'Enter') {
+                setTimeout(function() {
+                    if (intResultsNum.innerText === "0" && globalResultsNum.innerText >= "1") {
+                        document.querySelector('#globalResultsTab').classList.add('highlight-tab');
+                    } else {
+                        document.querySelector('#globalResultsTab').classList.remove('highlight-tab');
+                    }
+                    document.querySelector(".stats-row").style.visibility = "visible";
+                }, 500);
+            }
+        })
+    }
 });
