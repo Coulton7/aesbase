bf6a0c71ddaddc9575d2cebd53a77c0f830369eb
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-03-27 12:19:53 +0000

    Getting the expaning icons ready

---
+++ assets/js/expanding-icon.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const enquiryIcon = document.querySelector('.enquiry-icon');
+    const enquiryIconFa = enquiryIcon.querySelector('i');
+    const searchIcon = document.querySelector('.search-icon');
+    const feedbackIcon = document.querySelector('.feedback-icon');
+    const chatIcon = document.querySelector('.chat-icon');
+
+    searchIcon.style.display = "none";
+    feedbackIcon.style.display = "none";
+    if(chatIcon){
+        chatIcon.style.display = "none";
+    }
+
+    let isOpen = false;
+
+    enquiryIcon.addEventListener("click", function(event){
+    event.preventDefault()  
+
+        if (!isOpen) {
+            searchIcon.style.display = "flex";
+            feedbackIcon.style.display = "flex";
+            if(chatIcon){
+                chatIcon.style.display = "flex";
+            }
+        
+            // Add animation classes
+            searchIcon.classList.add('slide-up-btn-3'); 
+            feedbackIcon.classList.add('slide-up-btn-2'); 
+            if(chatIcon){
+                chatIcon.classList.add('slide-up-btn-1');
+            }
+        
+            enquiryIconFa.classList.remove('fa-question');
+            enquiryIconFa.classList.add('fa-times');
+            isOpen = true;
+        } else {
+            // Remove animation classes and hide buttons
+            searchIcon.classList.remove('slide-up-btn-3');
+            feedbackIcon.classList.remove('slide-up-btn-2');
+            if(chatIcon){
+                chatIcon.classList.remove('slide-up-btn-1');
+            }
+        
+            searchIcon.style.display = "none";
+            feedbackIcon.style.display = "none";
+            if(chatIcon){
+                chatIcon.style.display = "none";
+            }
+        
+            enquiryIconFa.classList.remove('fa-times');
+            enquiryIconFa.classList.add('fa-question');
+            isOpen = false;
+        }
+    });
+});
