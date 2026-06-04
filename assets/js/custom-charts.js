0181236c41eaeb441f6e25ba5b65109502773798
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-06-04 15:00:33 +0100

    Adding some optional changes for the graphs to fit on

--- assets/js/custom-charts.js
+++ assets/js/custom-charts.js
@@ -23,21 +23,38 @@ document.addEventListener("DOMContentLoaded", function() {
         },
         
         data: {
-        labels: ["1"],
+            labels: ["1"],
 
-        datasets: [{
-            data: [49.55],
-            backgroundColor: "rgba(63,103,126,1)",
-            hoverBackgroundColor: "rgba(50,90,100,1)"
-        },{
-            data: [18.04],
-            backgroundColor: "rgba(163,103,126,1)",
-            hoverBackgroundColor: "rgba(140,85,100,1)"
-        },{
-            data: [32.41],
-            backgroundColor: "rgba(63,203,226,1)",
-            hoverBackgroundColor: "rgba(46,185,235,1)"
-        }]
+            datasets: [{
+                data: [49.55],
+                backgroundColor: "rgba(63,103,126,1)",
+                hoverBackgroundColor: "rgba(50,90,100,1)"
+            },{
+                data: [18.04],
+                backgroundColor: "rgba(163,103,126,1)",
+                hoverBackgroundColor: "rgba(140,85,100,1)"
+            },{
+                data: [32.41],
+                backgroundColor: "rgba(63,203,226,1)",
+                hoverBackgroundColor: "rgba(46,185,235,1)"
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    grid: {
+                        borderColor: '#fff',
+                        color: '#fff'
+                    }
+                },
+                y: {
+                    grid: {
+                        borderColor: '#fff',
+                        color: '#fff'
+                    }
+                }
+            }
         }
     });
 
@@ -61,49 +78,66 @@ document.addEventListener("DOMContentLoaded", function() {
         },
         
         data: {
-        labels: ["live"],
+            labels: ["live"],
 
-        datasets: [{
-            data: [18.50],
-            backgroundColor: "rgba(63,103,126,1)",
-            hoverBackgroundColor: "rgba(50,90,100,1)"
-        },{
-            data: [0.04],
-            backgroundColor: "rgba(163,103,126,1)",
-            hoverBackgroundColor: "rgba(140,85,100,1)"
-        },{
-            data: [4.18],
-            backgroundColor: "rgba(63,203,226,1)",
-            hoverBackgroundColor: "rgba(46,185,235,1)"
-        },{
-            data: [2.10],
-            backgroundColor: "rgba(63,103,126,1)",
-            hoverBackgroundColor: "rgba(50,90,100,1)"
-        },{
-            data: [11.27],
-            backgroundColor: "rgba(163,103,126,1)",
-            hoverBackgroundColor: "rgba(140,85,100,1)"
-        },{
-            data: [3.08],
-            backgroundColor: "rgba(63,203,226,1)",
-            hoverBackgroundColor: "rgba(46,185,235,1)"
-        },{
-            data: [2.23],
-            backgroundColor: "rgba(63,103,126,1)",
-            hoverBackgroundColor: "rgba(50,90,100,1)"
-        },{
-            data: [6.85],
-            backgroundColor: "rgba(163,103,126,1)",
-            hoverBackgroundColor: "rgba(140,85,100,1)"
-        },{
-            data: [1.30],
-            backgroundColor: "rgba(63,203,226,1)",
-            hoverBackgroundColor: "rgba(46,185,235,1)"
-        },{
-            data: [50.45],
-            backgroundColor: "rgba(63,203,226,1)",
-            hoverBackgroundColor: "rgba(46,185,235,1)"
-        }]
+            datasets: [{
+                data: [18.50],
+                backgroundColor: "rgba(63,103,126,1)",
+                hoverBackgroundColor: "rgba(50,90,100,1)"
+            },{
+                data: [0.04],
+                backgroundColor: "rgba(163,103,126,1)",
+                hoverBackgroundColor: "rgba(140,85,100,1)"
+            },{
+                data: [4.18],
+                backgroundColor: "rgba(63,203,226,1)",
+                hoverBackgroundColor: "rgba(46,185,235,1)"
+            },{
+                data: [2.10],
+                backgroundColor: "rgba(63,103,126,1)",
+                hoverBackgroundColor: "rgba(50,90,100,1)"
+            },{
+                data: [11.27],
+                backgroundColor: "rgba(163,103,126,1)",
+                hoverBackgroundColor: "rgba(140,85,100,1)"
+            },{
+                data: [3.08],
+                backgroundColor: "rgba(63,203,226,1)",
+                hoverBackgroundColor: "rgba(46,185,235,1)"
+            },{
+                data: [2.23],
+                backgroundColor: "rgba(63,103,126,1)",
+                hoverBackgroundColor: "rgba(50,90,100,1)"
+            },{
+                data: [6.85],
+                backgroundColor: "rgba(163,103,126,1)",
+                hoverBackgroundColor: "rgba(140,85,100,1)"
+            },{
+                data: [1.30],
+                backgroundColor: "rgba(63,203,226,1)",
+                hoverBackgroundColor: "rgba(46,185,235,1)"
+            },{
+                data: [50.45],
+                backgroundColor: "rgba(63,203,226,1)",
+                hoverBackgroundColor: "rgba(46,185,235,1)"
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    grid: {
+                        borderColor: '#fff',
+                        color: '#fff'
+                    }
+                },
+                y: {
+                    grid: {
+                        borderColor: '#fff',
+                        color: '#fff'
+                    }
+                }
+            }
         }
     });
 });
