f19a25af529f6c9a9a741996be5a9b9555be3a11
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-07-02 16:22:56 +0100

    Trying new code to see the result

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1471,7 +1471,7 @@ document.addEventListener("DOMContentLoaded", function() {
                                         <p class=${data.summary ? '' : 'd-none'}>${data.summary}</p>
                                         <a class="${data.field_link_to_content ? '' : 'd-none'} btn btn-primary view-details align-self-end" href="${data.field_link_to_content}">${data.field_post_type != "youtube" ? 'Read More' : 'Watch Video'}</a>
                                         <a class="${data.type != "Locations" ? '' : 'd-none'} ${data.field_website_link ? '' : 'd-none'} btn btn-primary view-details align-self-end" href="${data.field_website_link}">Read More</a>                       
-                                        <a class="${data.type != "Announcements" ? '' : 'd-none'} btn btn-primary view-details align-self-end" href="${data.url}">${data.type != "Video" ? 'Read More' : 'Watch Video'}</a>
+                                        <a class="${data.type != "Announcements" ? '' : 'd-none'} ${data.type != "Statements and Compliance" ? '' : 'd-none'} btn btn-primary view-details align-self-end" href="${data.url}">${data.type != "Video" ? 'Read More' : 'Watch Video'}</a>
                                         <a class="${data.type == "Statements and Compliance" ? '' : 'd-none'} btn btn-primary view-details align-self-end" href="https://www.aesseal.com/en/resources/statements-and-compliance/">Read More</a>
                                     </div>
                                     <div class="col-md-3 col-4">
