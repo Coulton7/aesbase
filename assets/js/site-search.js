218119427be44581bc8f99f4f91b2e2357705121
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-07-07 12:36:21 +0100

    Trying a new variation of the code to see the result also adding the decode variation as a test

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1309,7 +1309,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         const queryParameters = {};
 
                         if(routeState.q) {
-                            queryParameters.q = routeState.q;
+                            queryParameters.q = routeState.q.split(' ').map(encodeURIComponent).join('+');
                         }
                         if(routeState.type) {
                             queryParameters.type = routeState.type.map(encodeURIComponent);
@@ -1338,7 +1338,7 @@ document.addEventListener("DOMContentLoaded", function() {
                             ? lang
                             :[lang].filter(Boolean);
                         return {
-                            q: decodeURIComponent(q),
+                            q: q.split('+').map(decodeURIComponent).join(' '),
                             type: allType.map(decodeURIComponent),
                             lang: allLang.map(decodeURIComponent),
                             page
