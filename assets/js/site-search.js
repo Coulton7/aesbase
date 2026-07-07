69c28f00cd6f3b9811a98189e30e5cfe3af91a6e
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-07-07 12:33:41 +0100

    Trying with some adjustments to the value

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1309,7 +1309,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         const queryParameters = {};
 
                         if(routeState.q) {
-                            queryParameters.q = encodeURIComponent(routeState.q);
+                            queryParameters.q = routeState.q;
                         }
                         if(routeState.type) {
                             queryParameters.type = routeState.type.map(encodeURIComponent);
@@ -1321,7 +1321,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         const queryString = qsModule.stringify(queryParameters, {
                             addQueryPrefix: true,
                             arrayFormat: 'indices',
-                            encodeValuesOnly: true
+                            encode: true
                         });
 
                         return `${origin}${pathname}${queryString}`;
