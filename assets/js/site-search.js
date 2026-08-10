dcbad5a4cdf88f51922982f2262a6ecc5df83672
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-08-10 12:19:30 +0100

    Not worked code overwritten

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -4061,13 +4061,15 @@ document.addEventListener("DOMContentLoaded", function() {
                             revealForm();
                             return html`<div class="no-result"><p class="h3">No results found matching ${results.query}</p>
                             <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>
-                            </div>`;
+                            </div>
+                            <p class="h3">Are you searching for a Part Number or Serial Number?</p>`;
                         }
                         else if(filterLang == '') {
                             revealForm();
                             return html`<div class="no-result"><p class="h3">No results found matching ${results.query}</p>
                             <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>
-                            </div>`;
+                            </div>
+                            <p class="h3">Are you searching for a Part Number or Serial Number?</p>`;
                         }
                         else if (filterLang == 'fr') {
                             revealForm();
