098cc652098d5e7ebebffb3c6238e4c9df2aff09
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-08-10 14:31:06 +0100

    Start of new no results on fr site

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -3742,22 +3742,37 @@ document.addEventListener("DOMContentLoaded", function() {
                     empty(results, { html }){
                         if(filterLang == 'en'){
                             revealForm();
-                            return html`<div class="no-result"><p class="h3">No results found matching ${results.query}</p>
+                            return html`<div class="no-result"><p class="h3">No results found matching ${results.query} on FR Site</p>
+                            <p class="lead text-white">Have you looked at our product document search tab above? You may find what you are looking for there.</p>
                             <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>
+                                <div class="text-center  py-5">
+                                    <p class="lead text-white">Would you like to search our Global site?</p>
+                                    <a href="https://www.aesseal.com/en/search" class="btn btn-danger" target="_blank" rel="noopener">Search our Global site</a>
+                                </div>
                             </div>
                             <p class="h3">Are you searching for a Part Number or Serial Number?</p>`;
                         }
                         else if(filterLang == '') {
                             revealForm();
-                            return html`<div class="no-result"><p class="h3">No results found matching ${results.query}</p>
+                            return html`<div class="no-result"><p class="h3">No results found matching ${results.query} on FR Site</p>
+                            <p class="lead text-white">Have you looked at our product document search tab above? You may find what you are looking for there.</p>
                             <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>
+                                <div class="text-center  py-5">
+                                    <p class="lead text-white">Would you like to search our Global site?</p>
+                                    <a href="https://www.aesseal.com/en/search" class="btn btn-danger" target="_blank" rel="noopener">Search our Global site</a>
+                                </div>
                             </div>
                             <p class="h3">Are you searching for a Part Number or Serial Number?</p>`;
                         }
                         else if (filterLang == 'fr') {
                             revealForm();
-                            return html`<div class="no-result"><p class="h3">Aucun résultat trouvé correspondant ${results.query}</p>
+                            return html`<div class="no-result"><p class="h3">Aucun résultat trouvé correspondant ${results.query} sur le site FR</p>
+                            <p class="lead text-white">Avez-vous consulté l'onglet Recherche de fiches produits ci-dessus ? Vous y trouverez peut-être ce que vous cherchez.</p>
                             <p>Nous sommes désolés de ne pas avoir trouvé de résultat pour votre recherche. Essayez d'effectuer une nouvelle recherche en vérifiant les fautes d'orthographe et/ou en réduisant le nombre de mots-clés utilisés. Vous pouvez également essayer d'utiliser une phrase de recherche plus large.</p>
+                            <div class="text-center  py-5">
+                                    <p class="lead text-white">Souhaitez-vous effectuer une recherche sur notre site international?</p>
+                                    <a href="https://www.aesseal.com/en/search" class="btn btn-danger" target="_blank" rel="noopener">Rechercher sur notre site international</a>
+                                </div>
                             </div>
                             <p class="h3">Vous recherchez un numéro de pièce ou un numéro de série ?</p>`;
                         }
