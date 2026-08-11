282fcc8df187587549d2bb044a99870844f4efed
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-08-11 14:37:02 +0100

    Removing the langlist panel for now as most content does not have translation

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -4972,21 +4972,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 container: document.querySelector('#clear-refinements'),
             }),
 
-            langlistPanel({
-                container: '#lang-list',
-                attribute: 'search_api_language',
-                templates: {
-                    item: '<input type="checkbox" data-insights-filter="${`search_api_language:${value}`}" class="ais-refinement-list--checkbox lang-item" value="{{label}}" {{#isRefined}}checked="true"{{/isRefined}}> {{label}} <span class="ais-refinement-list--count">({{count}})</span>',
-                },
-                transformItems(items){
-                    return items.map(item => ({
-                        ...item,
-                        label: langMapping[item.label],
-                    }));
-                },
-                sortBy: ['isRefined', 'count:desc', 'name:asc']
-            }),
-
             typelistPanel({
                 container: '#type-list',
                 attribute: 'type',
