18fee5c5d5c366cb195bf861547faaa1dbeee702
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-09-11 14:45:55 +0100

    Small adjustment to the no results text on main tab

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -2140,8 +2140,8 @@ document.addEventListener("DOMContentLoaded", function() {
                     empty(results, { html }){
                         revealForm();
                         return html`<p class="h3 mt-7">Please refine the search for ${results.query}</p>
-                    <p>Have you looked at our product document search tab above? You may find what you are looking for there.</p>
-                    <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>
+                    <p>Sorry we couldn’t find a result for your search. Have you tried using the product document search tab above?</p>
+                    <p>Alternatively please try again by checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>
                     <div class="pt-2">
                         <p class="h3">Would you like to search our Global site?</p>
                         <a href="https://www.aesseal.com/en/search" class="btn btn-outline-light mb-3" target="_blank" rel="noopener">Search our Global site</a>
