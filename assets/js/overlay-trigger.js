07882906a8f68a76481900af197566d0f43bad19
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-09-18 10:53:19 +0100

    Small format adjustment as a test

--- assets/js/overlay-trigger.js
+++ assets/js/overlay-trigger.js
@@ -256,7 +256,7 @@ document.addEventListener("DOMContentLoaded", function () {
       
     };
 
-    if(hash === "#documents" &&overlayDocumentBtn) {
+    if(hash === "#documents" && overlayDocumentBtn) {
       overlayDocumentBtn.addEventListener("click", function documentBtn() {
         overlayDocumentBtn.classList.toggle("change");
         if (d.style.height === "100%") {
