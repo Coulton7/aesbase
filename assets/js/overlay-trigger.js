7b4cf8723280ed238e6fb7732df699c15f497ea8
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-09-18 11:19:22 +0100

    Trying another variation of code to get the result

--- assets/js/overlay-trigger.js
+++ assets/js/overlay-trigger.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var query = 'q';
   var popSearch = document.querySelector("#popular-search-bar");
   var prefillBtns = document.querySelectorAll(".prefill-btn");
-  const hash = location.hash;
+  var hash = window.parent.location.hash;
   var popSearchInput = document.querySelector("#popular-search-input");
   var popSearchButton = document.querySelector(".pop-search-button");
 
