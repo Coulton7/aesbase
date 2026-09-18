42b554926a0ef70b0530f21023657959680caeb7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-09-18 11:11:40 +0100

    Trying popstate again instead

--- assets/js/overlay-trigger.js
+++ assets/js/overlay-trigger.js
@@ -287,7 +287,7 @@ document.addEventListener("DOMContentLoaded", function () {
           overlayDocumentBtn.classList.remove("document-btn");
         }
       });
-      window.addEventListener("hashchange", function(event) {
+      window.addEventListener("popstate", function(event) {
         console.log(hash);
         if(hash === "documents") {
           overlayDocumentBtn.classList.toggle("change");
