415d3119cf2a24e5db9cbe0d36d7aee4cb2a6679
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-09-18 11:03:27 +0100

    Changing to hashchange and adding a console.log listener

--- assets/js/overlay-trigger.js
+++ assets/js/overlay-trigger.js
@@ -287,7 +287,8 @@ document.addEventListener("DOMContentLoaded", function () {
           overlayDocumentBtn.classList.remove("document-btn");
         }
       });
-      window.addEventListener("popstate", function(event) {
+      window.addEventListener("hashchange", function(event) {
+        console.log(hash);
         if(hash === "documents") {
           overlayDocumentBtn.classList.toggle("change");
           if (d.style.height === "100%") {
